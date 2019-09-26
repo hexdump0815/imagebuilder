1c18ad65719963a3cfab08055ae41eab1d5eaa79
hexdump <hexdump0815@googlemail.com>
2019-09-26 17:42:38 +0200

    pin the releases and tags for more consistency

--- scripts/get-files.sh
+++ scripts/get-files.sh
@@ -14,40 +14,46 @@ fi
 
 cd `dirname $0`/..
 
+chromebook_snow_release_version="5.3.1-stb-cbe%2B"
+chromebook_snow_tree_tag="v5.3.1"
+
+odroid_u3_release_version="5.3.1-stb-exy%2B"
+odroid_u3_tree_tag="v5.3.1"
+
 if [ "$1" = "all" ] || [ "$1" = "chromebook_snow" ]; then
   rm -f boot/kernel-chromebook_snow-armv7l.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/releases/download/5.3.1-stb-cbe%2B/5.3.1-stb-cbe+.tar.gz -O boot/kernel-chromebook_snow-armv7l.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/releases/download/${chromebook_snow_release_version}/${chromebook_snow_release_version}.tar.gz -O boot/kernel-chromebook_snow-armv7l.tar.gz
   rm -f boot/boot-chromebook_snow-armv7l.dd
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/master/misc.cbe/boot/boot-chromebook_snow-armv7l.dd -O boot/boot-chromebook_snow-armv7l.dd
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/${chromebook_snow_tree_tag}/misc.cbe/boot/boot-chromebook_snow-armv7l.dd -O boot/boot-chromebook_snow-armv7l.dd
   rm -f files/gl4es-armv7l-debian.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/master/misc.cbe/gl4es-armv7l-debian.tar.gz -O files/gl4es-armv7l-debian.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/${chromebook_snow_tree_tag}/misc.cbe/gl4es-armv7l-debian.tar.gz -O files/gl4es-armv7l-debian.tar.gz
   rm -f files/gl4es-armv7l-ubuntu.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/master/misc.cbe/gl4es-armv7l-ubuntu.tar.gz -O files/gl4es-armv7l-ubuntu.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/${chromebook_snow_tree_tag}/misc.cbe/gl4es-armv7l-ubuntu.tar.gz -O files/gl4es-armv7l-ubuntu.tar.gz
   rm -f files/xorg-armsoc-armv7l-debian.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/master/misc.cbe/xorg-armsoc-armv7l-debian.tar.gz -O files/xorg-armsoc-armv7l-debian.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/${chromebook_snow_tree_tag}/misc.cbe/xorg-armsoc-armv7l-debian.tar.gz -O files/xorg-armsoc-armv7l-debian.tar.gz
   rm -f files/xorg-armsoc-armv7l-ubuntu.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/master/misc.cbe/xorg-armsoc-armv7l-ubuntu.tar.gz -O files/xorg-armsoc-armv7l-ubuntu.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/${chromebook_snow_tree_tag}/misc.cbe/xorg-armsoc-armv7l-ubuntu.tar.gz -O files/xorg-armsoc-armv7l-ubuntu.tar.gz
   rm -f files/opengl-chromebook_snow-armv7l.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/master/misc.cbe/opt-mali-exynos5250.tar.gz -O files/opengl-chromebook_snow-armv7l.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/${chromebook_snow_tree_tag}/misc.cbe/opt-mali-exynos5250.tar.gz -O files/opengl-chromebook_snow-armv7l.tar.gz
   rm -f files/opengl-fbdev-chromebook_snow-armv7l.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/master/misc.cbe/opt-mali-exynos5250-fbdev-r5p0.tar.gz -O files/opengl-fbdev-chromebook_snow-armv7l.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-samsung-snow-chromebook/raw/${chromebook_snow_tree_tag}/misc.cbe/opt-mali-exynos5250-fbdev-r5p0.tar.gz -O files/opengl-fbdev-chromebook_snow-armv7l.tar.gz
 fi
 
 if [ "$1" = "all" ] || [ "$1" = "odroid_u3" ]; then
   rm -f boot/kernel-odroid_u3-armv7l.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/releases/download/5.3.1-stb-exy%2B/5.3.1-stb-exy+.tar.gz -O boot/kernel-odroid_u3-armv7l.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/releases/download/${odroid_u3_release_version}/${odroid_u3_release_version}.tar.gz -O boot/kernel-odroid_u3-armv7l.tar.gz
   rm -f boot/boot-odroid_u3-armv7l.dd
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/master/misc.exy/u-boot/boot-odroid_u3-armv7l.dd -O boot/boot-odroid_u3-armv7l.dd
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/${odroid_u3_tree_tag}/misc.exy/u-boot/boot-odroid_u3-armv7l.dd -O boot/boot-odroid_u3-armv7l.dd
   rm -f files/gl4es-armv7l-debian.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/master/misc.exy/gl4es-armv7l-debian.tar.gz -O files/gl4es-armv7l-debian.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/${odroid_u3_tree_tag}/misc.exy/gl4es-armv7l-debian.tar.gz -O files/gl4es-armv7l-debian.tar.gz
   rm -f files/gl4es-armv7l-ubuntu.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/master/misc.exy/gl4es-armv7l-ubuntu.tar.gz -O files/gl4es-armv7l-ubuntu.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/${odroid_u3_tree_tag}/misc.exy/gl4es-armv7l-ubuntu.tar.gz -O files/gl4es-armv7l-ubuntu.tar.gz
   rm -f files/xorg-armsoc-armv7l-debian.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/master/misc.exy/xorg-armsoc-armv7l-debian.tar.gz -O files/xorg-armsoc-armv7l-debian.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/${odroid_u3_tree_tag}/misc.exy/xorg-armsoc-armv7l-debian.tar.gz -O files/xorg-armsoc-armv7l-debian.tar.gz
   rm -f files/xorg-armsoc-armv7l-ubuntu.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/master/misc.exy/xorg-armsoc-armv7l-ubuntu.tar.gz -O files/xorg-armsoc-armv7l-ubuntu.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/${odroid_u3_tree_tag}/misc.exy/xorg-armsoc-armv7l-ubuntu.tar.gz -O files/xorg-armsoc-armv7l-ubuntu.tar.gz
   rm -f files/opengl-odroid_u3-armv7l.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/master/misc.exy/opt-mali-exynos4412.tar.gz -O files/opengl-odroid_u3-armv7l.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/${odroid_u3_tree_tag}/misc.exy/opt-mali-exynos4412.tar.gz -O files/opengl-odroid_u3-armv7l.tar.gz
   rm -f files/opengl-fbdev-odroid_u3-armv7l.tar.gz
-  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/master/misc.exy/opt-mali-exynos4412-fbdev.tar.gz -O files/opengl-fbdev-odroid_u3-armv7l.tar.gz
+  wget -nv https://github.com/hexdump0815/linux-mainline-and-mali-on-odroid-u3/raw/${odroid_u3_tree_tag}/misc.exy/opt-mali-exynos4412-fbdev.tar.gz -O files/opengl-fbdev-odroid_u3-armv7l.tar.gz
 fi
