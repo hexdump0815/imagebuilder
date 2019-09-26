060799357077c3a311de5233aeec5399e595f90b
hexdump <hexdump0815@googlemail.com>
2019-09-26 18:53:36 +0200

    add postinstall for snow debian

---
+++ files/postinstall-chromebook_snow-armv7l-debian.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "" >> etc/pulse/default.pa
+echo "# required for working pulseaudio on snow" >> etc/pulse/default.pa
+echo "load-module module-alsa-sink device=sysdefault" >> etc/pulse/default.pa
