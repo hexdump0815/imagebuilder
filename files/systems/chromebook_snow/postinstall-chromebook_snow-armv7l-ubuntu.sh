0f3bd3dc2d067d7faaa9092f9bb12ecb2d4cce59
hexdump <hexdump0815@googlemail.com>
2020-12-28 09:55:44 +0100

    add ucm v1 compat for ubuntu focal

--- files/systems/chromebook_snow/postinstall-chromebook_snow-armv7l-ubuntu.sh
+++ files/systems/chromebook_snow/postinstall-chromebook_snow-armv7l-ubuntu.sh
@@ -4,3 +4,6 @@ echo "" >> etc/pulse/default.pa
 echo "# required for working pulseaudio on snow - audio input does not yet work well" >> etc/pulse/default.pa
 echo "load-module module-alsa-sink device=sysdefault" >> etc/pulse/default.pa
 echo "#load-module module-alsa-source device=sysdefault" >> etc/pulse/default.pa
+
+# enable ucm v1 backwards compatibility for the snow ucm files
+sed -i 's,Define.V1\ "",Define.V1 yes,g' usr/share/alsa/ucm2/ucm.conf
