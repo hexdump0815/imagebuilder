37f5549a9b260cf9a6e8d39ab034e07d7a1b8d65
hexdump <hexdump0815@googlemail.com>
2021-02-07 13:19:43 +0100

    move versions into system (part 3 - final)

--- systems/raspberry_pi_2/get-files.sh
+++ systems/raspberry_pi_2/get-files.sh
@@ -1,4 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
+raspberry_pi_2_release_version="5.10.3-stb-av7%2B"
+
 rm -f ${DOWNLOAD_DIR}/kernel-raspberry_pi-armv7l.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-stable-kernel/releases/download/${raspberry_pi_2_release_version}/${raspberry_pi_2_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-raspberry_pi-armv7l.tar.gz
--- systems/rockchip_rk322x/get-files.sh
+++ systems/rockchip_rk322x/get-files.sh
@@ -1,6 +1,10 @@
 # this file is supposed to be sourced by the get-files shell script
 
+rockchip_rk322x_release_version="4.4.194-rkc-322"
+rockchip_rk322x_tree_tag="e8c89a16da9860c600e49772db3b7f01331624b9"
+
 rm -f ${DOWNLOAD_DIR}/kernel-rockchip_rk322x-armv7l.tar.gz
 wget -v https://github.com/hexdump0815/linux-rockchip-rk322x-kernel/releases/download/${rockchip_rk322x_release_version}/${rockchip_rk322x_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-rockchip_rk322x-armv7l.tar.gz
+
 rm -f ${DOWNLOAD_DIR}/boot-rockchip_rk322x-armv7l.dd
 wget -v https://github.com/hexdump0815/linux-rockchip-rk322x-kernel/raw/${rockchip_rk322x_tree_tag}/misc.322/u-boot/boot-rockchip_rk3229_r39_4k-armv7l.dd -O ${DOWNLOAD_DIR}/boot-rockchip_rk322x-armv7l.dd
