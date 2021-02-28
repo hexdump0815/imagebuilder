dd10b5342f761b3519b760a4f153b3c34ef8d014
hexdump <hexdump0815@googlemail.com>
2021-02-28 11:20:38 +0100

    initial bits for the elm chromebook (acer r13)

---
+++ systems/chromebook_elm/postinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/15-swcursor.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
