90f8c1ce3c51165857086951e24aba5f1d4f5f8f
hexdump0815 <hexdump0815@googlemail.com>
2021-03-14 17:49:39 +0100

    fix elm and kukui kernel extraction

--- systems/chromebook_elm/get-files.sh
+++ systems/chromebook_elm/get-files.sh
@@ -5,4 +5,4 @@ chromebook_elm_release_version="5.10.20-stb-mt7%2B"
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_elm-aarch64.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/${chromebook_elm_release_version}/${chromebook_elm_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_elm-aarch64.tar.gz
 
-( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_elm-aarch64.tar.gz boot/vmlinux.kpart-elm-${chromebook_elm_release_version} ; mv boot/vmlinux.kpart-elm-${chromebook_elm_release_version} boot-chromebook_elm-aarch64.dd ; rmdir boot )
+( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_elm-aarch64.tar.gz boot ; mv boot/vmlinux.kpart-elm-* boot-chromebook_elm-aarch64.dd ; rm -rf boot )
--- systems/chromebook_kukui/get-files.sh
+++ systems/chromebook_kukui/get-files.sh
@@ -5,4 +5,4 @@ chromebook_kukui_release_version="5.10.20-stb-cbm%2B"
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_kukui-aarch64.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/${chromebook_kukui_release_version}/${chromebook_kukui_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_kukui-aarch64.tar.gz
 
-( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_kukui-aarch64.tar.gz boot/vmlinux.kpart-krane-${chromebook_kukui_release_version} ; mv boot/vmlinux.kpart-krane-${chromebook_kukui_release_version} boot-chromebook_kukui-aarch64.dd ; rmdir boot )
+( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_kukui-aarch64.tar.gz boot ; mv boot/vmlinux.kpart-krane-* boot-chromebook_kukui-aarch64.dd ; rm -rf boot )
