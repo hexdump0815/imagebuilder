f44dd9803c0c1b930c05812a9bc8a8b9b9e2eedb
hexdump <hexdump0815@googlemail.com>
2021-03-14 17:16:16 +0100

    snow: work around broken alsa naming

--- systems/chromebook_snow/postinstall.sh
+++ systems/chromebook_snow/postinstall.sh
@@ -5,8 +5,5 @@ echo "# required for working pulseaudio on snow - audio input does not yet work 
 echo "load-module module-alsa-sink device=sysdefault" >> etc/pulse/default.pa
 echo "#load-module module-alsa-source device=sysdefault" >> etc/pulse/default.pa
 
-# enable ucm v1 backwards compatibility for the snow ucm files
-sed -i 's,Define.V1\ "",Define.V1 yes,g' usr/share/alsa/ucm2/ucm.conf
-
 cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
