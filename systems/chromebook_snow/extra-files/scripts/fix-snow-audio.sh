c45952d6be8b57a58cbef95c80096186d48f8554
hexdump <hexdump0815@googlemail.com>
2021-03-21 08:45:03 +0100

    move snow audio fix into its own script

---
+++ systems/chromebook_snow/extra-files/scripts/fix-snow-audio.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# work around the broken alsa naming
+ASOUND_SHORT_NAME=`cat /proc/asound/cards | awk '{print $3}' | head -n 1`
+ASOUND_LONG_NAME=`cat /proc/asound/cards | awk '{print $5}' | head -n 1`
+if [ "${ASOUND_SHORT_NAME}" != "" ] && [ "${ASOUND_LONG_NAME}" != "" ]; then
+  rm -rf /usr/share/alsa/ucm2/${ASOUND_SHORT_NAME}
+  cp -r /usr/share/alsa/ucm2/${ASOUND_LONG_NAME} /usr/share/alsa/ucm2/${ASOUND_SHORT_NAME}
+fi
