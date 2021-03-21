2d6485abc4c84b7f5a1076ecc5bd20f224fde074
hexdump <hexdump0815@googlemail.com>
2021-03-21 16:29:22 +0100

    use latest kernel builds for aarch64 chromebooks

--- systems/chromebook_elm/get-files.sh
+++ systems/chromebook_elm/get-files.sh
@@ -1,6 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-chromebook_elm_release_version="5.10.20-stb-mt7%2B"
+chromebook_elm_release_version="5.10.25-stb-mt7%2B"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_elm-aarch64.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/${chromebook_elm_release_version}/${chromebook_elm_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_elm-aarch64.tar.gz
--- systems/chromebook_kukui/get-files.sh
+++ systems/chromebook_kukui/get-files.sh
@@ -1,6 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-chromebook_kukui_release_version="5.10.20-stb-cbm%2B"
+chromebook_kukui_release_version="5.10.25-stb-cbm%2B"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_kukui-aarch64.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/${chromebook_kukui_release_version}/${chromebook_kukui_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_kukui-aarch64.tar.gz
