77e65661ebe81fb9d427858b4c5584a0e4e73459
hexdump <hexdump0815@googlemail.com>
2021-03-21 08:44:30 +0100

    get proper u-boot version with sd card detect fix

--- systems/chromebook_snow/get-files.sh
+++ systems/chromebook_snow/get-files.sh
@@ -1,7 +1,7 @@
 # this file is supposed to be sourced by the get-files shell script
 
 chromebook_snow_release_version="5.10.3-stb-cbe%2B"
-chromebook_snow_uboot_version="v2020.04-cbe"
+chromebook_snow_uboot_version="v2018.11-cbe"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_snow-armv7l.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-stable-kernel/releases/download/${chromebook_snow_release_version}/${chromebook_snow_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_snow-armv7l.tar.gz
