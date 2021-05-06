26d8a90a54490761f1bc601058c1134026563c24
hexdump <hexdump0815@googlemail.com>
2021-05-06 12:41:57 +0200

    prepare for mesa 21.1.0

--- systems/chromebook_nyan/get-files.sh
+++ systems/chromebook_nyan/get-files.sh
@@ -2,6 +2,7 @@
 
 chromebook_nyan_release_version="5.10.25-stb-cbt%2B"
 chromebook_nyan_uboot_version="v2018.11-cbt"
+xserver_release_version="21.0.1"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_nyan-armv7l.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-stable-kernel/releases/download/${chromebook_nyan_release_version}/${chromebook_nyan_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_nyan-armv7l.tar.gz
@@ -9,10 +10,6 @@ wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-stable-ke
 rm -f ${DOWNLOAD_DIR}/boot-chromebook_nyan-armv7l.dd
 wget -v https://github.com/hexdump0815/u-boot-chainloading-for-arm-chromebooks/releases/download/${chromebook_nyan_uboot_version}/uboot.kpart.cbt.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-chromebook_nyan-armv7l.dd
 
-# as long as there is a separate build for the tegra mesa, get it here - will disappear soon ...
-rm -f ${DOWNLOAD_DIR}/opt-mesa-${3}-${2}.tar.gz
-wget https://github.com/hexdump0815/mesa-etc-build/releases/download/${mesa_release_version}/opt-mesa-tegra-${mesa_release_version}-${3}-${2}.tar.gz -O ${DOWNLOAD_DIR}/opt-mesa-${3}-${2}.tar.gz
-
 # on tegra a fresher xorg is required as well
 rm -f ${DOWNLOAD_DIR}/opt-xserver-${3}-${2}.tar.gz
-wget https://github.com/hexdump0815/mesa-etc-build/releases/download/${mesa_release_version}/opt-xserver-${mesa_release_version}-${3}-${2}.tar.gz -O ${DOWNLOAD_DIR}/opt-xserver-${3}-${2}.tar.gz
+wget https://github.com/hexdump0815/mesa-etc-build/releases/download/${xserver_release_version}/opt-xserver-${xserver_release_version}-${3}-${2}.tar.gz -O ${DOWNLOAD_DIR}/opt-xserver-${3}-${2}.tar.gz
