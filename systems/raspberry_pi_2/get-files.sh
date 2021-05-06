ad8b039daec783ac45cb2f23daf59e9ca705a9e2
hexdump <hexdump0815@googlemail.com>
2021-05-06 12:14:05 +0200

    rpi u-boot.bin cleanup

--- systems/raspberry_pi_2/get-files.sh
+++ systems/raspberry_pi_2/get-files.sh
@@ -1,6 +1,10 @@
 # this file is supposed to be sourced by the get-files shell script
 
 raspberry_pi_2_release_version="5.10.25-stb-av7%2B"
+raspberry_pi_2_uboot_version="201216-01"
 
 rm -f ${DOWNLOAD_DIR}/kernel-raspberry_pi-armv7l.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-stable-kernel/releases/download/${raspberry_pi_2_release_version}/${raspberry_pi_2_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-raspberry_pi_2-armv7l.tar.gz
+
+mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
+wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${raspberry_pi_3_uboot_version}/rpi_2-u-boot.bin.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/rpi_2-u-boot.bin
--- systems/raspberry_pi_3/postinstall.sh
+++ systems/raspberry_pi_3/postinstall.sh
@@ -3,6 +3,5 @@
 cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
 
 mv -v boot/extra/rpi_3-u-boot.bin boot/kernel8.img
-# TODO: disabled for now this u-boot version still has to be built
-#mv -v boot/extra/rpi_3_plus-u-boot.bin boot/kernel8.img-rpi3plus
+mv -v boot/extra/rpi_3_plus-u-boot.bin boot/kernel8.img-rpi3plus
 rmdir boot/extra
