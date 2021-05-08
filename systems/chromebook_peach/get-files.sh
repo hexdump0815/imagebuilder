eec2576192d0d58328fcc0417cb11dec7c9fedb1
hexdump <hexdump0815@googlemail.com>
2021-05-08 11:49:25 +0200

    proper peach pit and peach pi u-boot

--- systems/chromebook_peach/get-files.sh
+++ systems/chromebook_peach/get-files.sh
@@ -1,7 +1,8 @@
 # this file is supposed to be sourced by the get-files shell script
 
 chromebook_peach_release_version="5.10.27-stb-cbp%2B"
-chromebook_peach_uboot_version="v2017.09-cbp"
+chromebook_peach_pit_uboot_version="v2017.09-cbp"
+chromebook_peach_pi_uboot_version="v2017.09-cbp"
 chromebook_peach_mali_blob_version="4b25cd43f7aa853f35b17033b713d46e785c11cd"
 gl4es_focal_armv7l_version="9051dfe1f2198e2ed41c322359ee8324043d55a9"
 
@@ -12,7 +13,13 @@ rm -f ${DOWNLOAD_DIR}/kernel-mali-chromebook_peach-armv7l.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-stable-kernel/releases/download/${chromebook_peach_release_version}/${chromebook_peach_release_version}-mali-exynos5250.tar.gz -O ${DOWNLOAD_DIR}/kernel-mali-chromebook_peach-armv7l.tar.gz
 
 rm -f ${DOWNLOAD_DIR}/boot-chromebook_peach-armv7l.dd
-wget -v https://github.com/hexdump0815/u-boot-chainloading-for-arm-chromebooks/releases/download/${chromebook_peach_uboot_version}/uboot.kpart.cbe-peach.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-chromebook_peach-armv7l.dd
+wget -v https://github.com/hexdump0815/u-boot-chainloading-for-arm-chromebooks/releases/download/${chromebook_peach_pit_uboot_version}/uboot.kpart.cbe-peach-pit.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-chromebook_peach-armv7l.dd
+
+# get different u-boot versions for different exynos5250 chromebook versions to have them around
+rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
+mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
+cp ${DOWNLOAD_DIR}/boot-chromebook_peach-armv7l.dd ${DOWNLOAD_DIR}/boot-extra-${1}/uboot.kpart.cbe-peach-pit
+wget -v https://github.com/hexdump0815/u-boot-chainloading-for-arm-chromebooks/releases/download/${chromebook_peach_pi_uboot_version}/uboot.kpart.cbe-peach-pi.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/uboot.kpart.cbe-peach-pi
 
 rm -f ${DOWNLOAD_DIR}/opengl-chromebook_peach-armv7l.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-exynos5422-kernel/raw/${chromebook_peach_mali_blob_version}/misc.e54/opt-mali-exynos5422-armv7l.tar.gz -O ${DOWNLOAD_DIR}/opengl-chromebook_peach-armv7l.tar.gz
