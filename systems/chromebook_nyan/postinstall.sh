0223408bf7921233427ff7d3385ecb10df13b6b3
hexdump <hexdump0815@googlemail.com>
2021-06-13 07:32:30 +0200

    the own mesa is used by default now if its there

--- systems/chromebook_nyan/postinstall.sh
+++ systems/chromebook_nyan/postinstall.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "/opt/mesa-armv7l/lib/arm-linux-gnueabihf" > etc/ld.so.conf.d/aaa-mesa.conf
-
-mv usr/lib/arm-linux-gnueabihf/dri usr/lib/arm-linux-gnueabihf/dri.org
-ln -s /opt/mesa-armv7l/lib/arm-linux-gnueabihf/dri usr/lib/arm-linux-gnueabihf/dri
-
 echo "" >> etc/pulse/default.pa
 echo "# required for working pulseaudio on nyan big - audio input does not yet work well" >> etc/pulse/default.pa
 echo "load-module module-alsa-sink device=hw:1" >> etc/pulse/default.pa
