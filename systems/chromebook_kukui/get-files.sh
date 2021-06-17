7b87f1a98cf32ec3e1bd04b09628b048c7119dfb
hexdump <hexdump0815@googlemail.com>
2021-06-17 23:16:23 +0200

    proper names here are oak and kukui - add hack for now

--- systems/chromebook_kukui/get-files.sh
+++ systems/chromebook_kukui/get-files.sh
@@ -5,4 +5,6 @@ chromebook_kukui_release_version="5.10.25-stb-cbm%2B"
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_kukui-aarch64.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/${chromebook_kukui_release_version}/${chromebook_kukui_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_kukui-aarch64.tar.gz
 
+# hack until there is a kernel package with the proper name
 ( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_kukui-aarch64.tar.gz boot ; mv boot/vmlinux.kpart-krane-* boot-chromebook_kukui-aarch64.dd ; rm -rf boot )
+#( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_kukui-aarch64.tar.gz boot ; mv boot/vmlinux.kpart-kukui-* boot-chromebook_kukui-aarch64.dd ; rm -rf boot )
--- systems/chromebook_oak/get-files.sh
+++ systems/chromebook_oak/get-files.sh
@@ -5,4 +5,6 @@ chromebook_oak_release_version="5.10.25-stb-mt7%2B"
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_oak-aarch64.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/${chromebook_oak_release_version}/${chromebook_oak_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_oak-aarch64.tar.gz
 
-( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_oak-aarch64.tar.gz boot ; mv boot/vmlinux.kpart-oak-* boot-chromebook_oak-aarch64.dd ; rm -rf boot )
+# hack until there is a kernel package with the proper name
+( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_oak-aarch64.tar.gz boot ; mv boot/vmlinux.kpart-elm-* boot-chromebook_oak-aarch64.dd ; rm -rf boot )
+#( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_oak-aarch64.tar.gz boot ; mv boot/vmlinux.kpart-oak-* boot-chromebook_oak-aarch64.dd ; rm -rf boot )
