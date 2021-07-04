7ee72aa0d3a15fc5736d5a7e84386d5976aeac95
hexdump <hexdump0815@googlemail.com>
2021-07-04 17:27:54 +0200

    snow,nyan: put legacy kernel into /boot/extra

--- systems/chromebook_nyan/get-files.sh
+++ systems/chromebook_nyan/get-files.sh
@@ -6,6 +6,7 @@ chromebook_nyan_kernel_tree="linux-mainline-tegra-k1-kernel"
 # this one is for the v5.10 or newer kernel tree which still has very unreliable display handling
 #chromebook_nyan_release_version="5.10.25-stb-cbt%2B"
 #chromebook_nyan_kernel_tree="linux-mainline-and-mali-generic-stable-kernel"
+chromebook_nyan_legacy_release_version="3.10.18-cos-r91"
 chromebook_nyan_uboot_version="v2018.11-cbt"
 chromebook_nyan_2g_uboot_version="v2021.07-rc4-cbt"
 xserver_release_version="21.0.1"
@@ -16,11 +17,12 @@ wget -v https://github.com/hexdump0815/${chromebook_nyan_kernel_tree}/releases/d
 rm -f ${DOWNLOAD_DIR}/boot-chromebook_nyan-armv7l.dd
 wget -v https://github.com/hexdump0815/u-boot-chainloading-for-arm-chromebooks/releases/download/${chromebook_nyan_uboot_version}/uboot.kpart.cbt.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-chromebook_nyan-armv7l.dd
 
-# keep the u-boot image in the extra dir to have it around
+# keep the u-boot image and the legacy chromeos kernel in the extra dir to have it around
 rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
 mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
 cp ${DOWNLOAD_DIR}/boot-chromebook_nyan-armv7l.dd ${DOWNLOAD_DIR}/boot-extra-${1}/uboot.kpart.cbt-nyan-4g
 wget -v https://github.com/hexdump0815/u-boot-chainloading-for-arm-chromebooks/releases/download/${chromebook_nyan_2g_uboot_version}/uboot.kpart.cbt-2g.gz -O - | gunzip -c > ${DOWNLOAD_DIR}//boot-extra-${1}/uboot.kpart.cbt-nyan-2g
+wget -v https://github.com/hexdump0815/linux-chromeos-kernel/releases/download/${chromebook_nyan_legacy_release_version}/${chromebook_nyan_legacy_release_version}.tar.gz -O ${DOWNLOAD_DIR}/boot-extra-${1}/kernel-chromebook_nyan-armv7l-legacy.tar.gz
 
 # the v5.4 kernel does not work well together with the nouveau gpu driver, so this is not required yet
 ## on tegra a fresher xorg is required as well
