32c7d4f16cc1759bbb7746beeaac55f8505c429c
hexdump <hexdump0815@googlemail.com>
2021-07-22 19:38:19 +0200

    cgpt is required as well

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,4 +3,4 @@
 # install some packages required for the imagebuilder framework
 #
 
-apt-get install debootstrap liblz4-tool vboot-kernel-utils btrfs-progs
+apt-get install debootstrap liblz4-tool vboot-kernel-utils btrfs-progs cgpt
