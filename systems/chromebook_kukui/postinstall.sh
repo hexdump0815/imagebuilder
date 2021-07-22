4b016471f9c3f3ad11e45615d73183b3fec8ef33
hexdump <hexdump0815@googlemail.com>
2021-07-22 22:35:57 +0200

    kukui: try to confg proper monitor rotation on first boot

--- systems/chromebook_kukui/postinstall.sh
+++ systems/chromebook_kukui/postinstall.sh
@@ -2,5 +2,7 @@
 
 cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/13-panfrost-swapped-dri-nodes.conf etc/X11/xorg.conf.d
-cp -v etc/X11/xorg.conf.d.samples/31-monitor-no-dpms-rotate-left.conf etc/X11/xorg.conf.d
+# the correct monitor rotation config file now gets installed on first boot via /etc/rc.local
+#cp -v etc/X11/xorg.conf.d.samples/31-monitor-no-dpms.conf etc/X11/xorg.conf.d
+#cp -v etc/X11/xorg.conf.d.samples/31-monitor-no-dpms-rotate-left.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
