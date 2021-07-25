d2f986ad21e8249ba9933bad6d7538814f8fc3be
hexdump <hexdump0815@googlemail.com>
2021-07-25 20:20:19 +0200

    snow: a try to hack the ucm to a working state

--- systems/chromebook_snow/extra-files/scripts/fix-snow-audio.sh
+++ systems/chromebook_snow/extra-files/scripts/fix-snow-audio.sh
@@ -7,3 +7,8 @@ if [ "${ASOUND_SHORT_NAME}" != "" ] && [ "${ASOUND_LONG_NAME}" != "" ]; then
   rm -rf /usr/share/alsa/ucm2/${ASOUND_SHORT_NAME}
   cp -r /usr/share/alsa/ucm2/${ASOUND_LONG_NAME} /usr/share/alsa/ucm2/${ASOUND_SHORT_NAME}
 fi
+
+# this is to make debian bullseye happy too
+mkdir -p /usr/share/alsa/ucm2/Samsung/snow
+cp /usr/share/alsa/ucm2/${ASOUND_SHORT_NAME}/HiFi.conf /usr/share/alsa/ucm2/Samsung/snow/HiFi.conf
+cp /usr/share/alsa/ucm2/${ASOUND_SHORT_NAME}/HiFi.conf /usr/share/alsa/ucm2/Samsung/snow/HiFi.conf.working
