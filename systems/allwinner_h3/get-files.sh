57269d465e4bf3c2bca175ad6c2325ceed7b65a9
hexdump <hexdump0815@googlemail.com>
2021-08-06 20:22:29 +0200

    allwinner h3: some fine tuning, more and cleanuo

--- systems/allwinner_h3/get-files.sh
+++ systems/allwinner_h3/get-files.sh
@@ -1,7 +1,8 @@
 # this file is supposed to be sourced by the get-files shell script
 
 allwinner_h3_release_version="5.10.25-stb-av7%2B"
-allwinner_h3_uboot_version="200718-01"
+allwinner_h3_r39_uboot_version="200718-01"
+allwinner_h3_tx1_uboot_version="200718-01"
 mesa_release_version="21.0.1"
 
 rm -f ${DOWNLOAD_DIR}/kernel-allwinner_h3-armv7l.tar.gz
@@ -11,7 +12,13 @@ wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-kernel/re
 #wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-kernel/releases/download/${allwinner_h3_release_version}/${allwinner_h3_release_version}-mali-sunxi.tar.gz -O ${DOWNLOAD_DIR}/kernel-mali-allwinner_h3-armv7l.tar.gz
 
 rm -f ${DOWNLOAD_DIR}/boot-allwinner_h3-armv7l.dd
-wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${allwinner_h3_uboot_version}/r39-boot.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-allwinner_h3-armv7l.dd
+wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${allwinner_h3_r39_uboot_version}/r39-boot.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-allwinner_h3-armv7l.dd
+
+# get different u-boot versions for different amlogic versions to have them around
+rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
+mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
+cp ${DOWNLOAD_DIR}/boot-allwinner_h3-${2}.dd ${DOWNLOAD_DIR}/boot-extra-${1}/boot-allwinner-h3-r39.dd
+wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${allwinner_h3_tx1_uboot_version}/tx1-boot.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-allwinner-h3-tx1.dd
 
 # get the self built fresher mesa
 wget https://github.com/hexdump0815/mesa-etc-build/releases/download/${mesa_release_version}/opt-mesa-${mesa_release_version}-${3}-${2}.tar.gz -O ${DOWNLOAD_DIR}/opt-mesa-${3}-${2}.tar.gz
--- systems/allwinner_h3/readme.md
+++ systems/allwinner_h3/readme.md
@@ -7,6 +7,7 @@
 ## tested systems - working
 
 - r39 start h3 tv box
+- tx1 h3 tv box
 
 ## untested systems
 
@@ -14,12 +15,22 @@ some more work will be required to make the devices below working
 
 - various orangepi h3 sbc's
 - various nanopi h3 sbc's
-- allwinner h3 based tv boxes
+- other allwinner h3 based tv boxes
 
 ## kernel build notes
 
 - https://github.com/hexdump0815/linux-mainline-and-mali-generic-stable-kernel/blob/master/readme.av7
 
+## u-boot build notes
+
+- https://github.com/hexdump0815/u-boot-misc/blob/master/readme.h3
+
 ## special notes
 
 - tv boxes are always hit and miss, so they might work or might not work and are usually of low quality
+  - be happy in case it works for your box
+  - it is impossible to support all tv boxes with their wild mix of hardware or usually quite low quality, so please do not open github issues in case the images do not work for your box
+  - github issues with tested and structured information on how to get the images working on a box where they did not work out of the box are welcome
+  - sound, wifi, bluetooth and sometimes even ethernet might work or might not work depending on the tv box
+- as allwinner socs always boot by default from sd card the emmc can safely be overwritten (but do a backup of the original android system via dd command beforehand - it might be useful later)
+- at least the r39 u-boot seems to not be perfect yet as sometimes it needs a few tries to boot successfully
