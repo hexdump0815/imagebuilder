53b90d1ea505248e04bebf43869566c16d15dde1
hexdump <hexdump0815@googlemail.com>
2021-08-08 13:27:21 +0200

    amlogic m8: somre required fixes for it to work

--- systems/amlogic_m8/postinstall.sh
+++ systems/amlogic_m8/postinstall.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# there is no hdmi supported you, so disable the lightdm display manager for now
+systemctl stop lightdm
+systemctl disable lightdm
+
+# just put some generic xorg.conf there although xorg does not make sense here yet
 cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
