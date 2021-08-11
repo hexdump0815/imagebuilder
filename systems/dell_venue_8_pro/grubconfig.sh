d1567c326cfc8621bac87cae6bf13761d92c2710
hexdump <hexdump0815@googlemail.com>
2021-08-11 23:11:56 +0200

    d8p: dell venue 8 pro is now in atom ...

--- systems/dell_venue_8_pro/grubconfig.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# add some extra options to the kernel cmdline
-sed -i 's,GRUB_CMDLINE_LINUX="",GRUB_CMDLINE_LINUX="rootwait fsck.repair=yes net.ifnames=0 ipv6.disable=1 systemd.gpt_auto=0 i915.enable_rc6=1 i915.enable_fbc=1 fbcon=rotate:1 intel_idle.max_cstate=2",g' /etc/default/grub
