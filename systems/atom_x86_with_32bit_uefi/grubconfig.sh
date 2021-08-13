7abe85aaf4009625650e8a05707a8193e42e4948
hexdump <hexdump0815@googlemail.com>
2021-08-13 09:06:08 +0200

    better screen rotation

--- systems/atom_x86_with_32bit_uefi/grubconfig.sh
+++ systems/atom_x86_with_32bit_uefi/grubconfig.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # add some extra options to the kernel cmdline
-sed -i 's,GRUB_CMDLINE_LINUX="",GRUB_CMDLINE_LINUX="rootwait fsck.repair=yes net.ifnames=0 ipv6.disable=1 systemd.gpt_auto=0 i915.enable_rc6=1 i915.enable_fbc=1 fbcon=rotate:1 intel_idle.max_cstate=2 rd.driver.blacklist=i915 rd.driver.blacklist=drm rd.driver.blacklist=drm_kms_helper",g' /etc/default/grub
+sed -i 's,GRUB_CMDLINE_LINUX="",GRUB_CMDLINE_LINUX="rootwait fsck.repair=yes net.ifnames=0 ipv6.disable=1 systemd.gpt_auto=0 i915.enable_rc6=1 i915.enable_fbc=1 video=DSI-1:panel_orientation=right_side_up intel_idle.max_cstate=2 rd.driver.blacklist=i915 rd.driver.blacklist=drm rd.driver.blacklist=drm_kms_helper",g' /etc/default/grub
