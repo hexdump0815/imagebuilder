9c13539ba819ccd5c1c62946cbb05764e2df531c
hexdump0815 <hexdump0815@googlemail.com>
2021-10-16 22:51:54 +0200

    some steps are not required with pmos kernel

--- scripts/create-image.sh
+++ scripts/create-image.sh
@@ -259,42 +259,44 @@ else
   echo $FSTAB_SWAP_PART >> ${MOUNT_POINT}/etc/fstab
 fi
 
-if [ "${2}" = "armv7l" ] || [ "${2}" = "aarch64" ]; then
-  export KERNEL_VERSION=`ls ${BUILD_ROOT}/boot/*Image-* | sed 's,.*Image-,,g' | sort -u`
-  if [ "$PARTUUID_ROOT" = "YES" ]; then
-    ROOT_PARTUUID=$(blkid | grep "/dev/loop0p$ROOTPART" | awk '{print $5}' | sed 's,",,g')
-    if [ -f ${MOUNT_POINT}/boot/extlinux/extlinux.conf ]; then
-      sed -i "s,ROOT_PARTUUID,$ROOT_PARTUUID,g" ${MOUNT_POINT}/boot/extlinux/extlinux.conf
-      sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/extlinux/extlinux.conf
-    fi
-    if [ -f ${MOUNT_POINT}/boot/menu/extlinux.conf ]; then
-      sed -i "s,ROOT_PARTUUID,$ROOT_PARTUUID,g" ${MOUNT_POINT}/boot/menu/extlinux.conf
-      sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/menu/extlinux.conf
-    fi
-    if [ -f ${MOUNT_POINT}/boot/r89-boot/parameter-linux ]; then
-      sed -i "s,ROOT_PARTUUID,$ROOT_PARTUUID,g" ${MOUNT_POINT}/boot/r89-boot/parameter-linux
-      sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/r89-boot/parameter-linux
-    fi
-    if [ -f ${MOUNT_POINT}/boot/uEnv.ini ]; then
-      sed -i "s,ROOT_PARTUUID,$ROOT_PARTUUID,g" ${MOUNT_POINT}/boot/uEnv.ini
-      sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/uEnv.ini
-    fi
-  else
-    if [ -f ${MOUNT_POINT}/boot/extlinux/extlinux.conf ]; then
-      sed -i "s,ROOT_PARTUUID,LABEL=$ROOTPARTLABEL,g" ${MOUNT_POINT}/boot/extlinux/extlinux.conf
-      sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/extlinux/extlinux.conf
-    fi
-    if [ -f ${MOUNT_POINT}/boot/menu/extlinux.conf ]; then
-      sed -i "s,ROOT_PARTUUID,LABEL=$ROOTPARTLABEL,g" ${MOUNT_POINT}/boot/menu/extlinux.conf
-      sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/menu/extlinux.conf
-    fi
-    if [ -f ${MOUNT_POINT}/boot/r89-boot/parameter-linux ]; then
-      sed -i "s,ROOT_PARTUUID,LABEL=$ROOTPARTLABEL,g" ${MOUNT_POINT}/boot/r89-boot/parameter-linux
-      sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/r89-boot/parameter-linux
-    fi
-    if [ -f ${MOUNT_POINT}/boot/uEnv.ini ]; then
-      sed -i "s,ROOT_PARTUUID,LABEL=$ROOTPARTLABEL,g" ${MOUNT_POINT}/boot/uEnv.ini
-      sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/uEnv.ini
+if [ "$PMOSKERNEL" != "true" ]; then
+  if [ "${2}" = "armv7l" ] || [ "${2}" = "aarch64" ]; then
+    export KERNEL_VERSION=`ls ${BUILD_ROOT}/boot/*Image-* | sed 's,.*Image-,,g' | sort -u`
+    if [ "$PARTUUID_ROOT" = "YES" ]; then
+      ROOT_PARTUUID=$(blkid | grep "/dev/loop0p$ROOTPART" | awk '{print $5}' | sed 's,",,g')
+      if [ -f ${MOUNT_POINT}/boot/extlinux/extlinux.conf ]; then
+        sed -i "s,ROOT_PARTUUID,$ROOT_PARTUUID,g" ${MOUNT_POINT}/boot/extlinux/extlinux.conf
+        sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/extlinux/extlinux.conf
+      fi
+      if [ -f ${MOUNT_POINT}/boot/menu/extlinux.conf ]; then
+        sed -i "s,ROOT_PARTUUID,$ROOT_PARTUUID,g" ${MOUNT_POINT}/boot/menu/extlinux.conf
+        sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/menu/extlinux.conf
+      fi
+      if [ -f ${MOUNT_POINT}/boot/r89-boot/parameter-linux ]; then
+        sed -i "s,ROOT_PARTUUID,$ROOT_PARTUUID,g" ${MOUNT_POINT}/boot/r89-boot/parameter-linux
+        sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/r89-boot/parameter-linux
+      fi
+      if [ -f ${MOUNT_POINT}/boot/uEnv.ini ]; then
+        sed -i "s,ROOT_PARTUUID,$ROOT_PARTUUID,g" ${MOUNT_POINT}/boot/uEnv.ini
+        sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/uEnv.ini
+      fi
+    else
+      if [ -f ${MOUNT_POINT}/boot/extlinux/extlinux.conf ]; then
+        sed -i "s,ROOT_PARTUUID,LABEL=$ROOTPARTLABEL,g" ${MOUNT_POINT}/boot/extlinux/extlinux.conf
+        sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/extlinux/extlinux.conf
+      fi
+      if [ -f ${MOUNT_POINT}/boot/menu/extlinux.conf ]; then
+        sed -i "s,ROOT_PARTUUID,LABEL=$ROOTPARTLABEL,g" ${MOUNT_POINT}/boot/menu/extlinux.conf
+        sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/menu/extlinux.conf
+      fi
+      if [ -f ${MOUNT_POINT}/boot/r89-boot/parameter-linux ]; then
+        sed -i "s,ROOT_PARTUUID,LABEL=$ROOTPARTLABEL,g" ${MOUNT_POINT}/boot/r89-boot/parameter-linux
+        sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/r89-boot/parameter-linux
+      fi
+      if [ -f ${MOUNT_POINT}/boot/uEnv.ini ]; then
+        sed -i "s,ROOT_PARTUUID,LABEL=$ROOTPARTLABEL,g" ${MOUNT_POINT}/boot/uEnv.ini
+        sed -i "s,KERNEL_VERSION,$KERNEL_VERSION,g" ${MOUNT_POINT}/boot/uEnv.ini
+      fi
     fi
   fi
 fi
