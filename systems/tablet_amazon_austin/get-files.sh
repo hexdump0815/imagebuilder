257b304c3f1954886c7fc1985357d4fd3435831e
hexdump0815 <hexdump0815@googlemail.com>
2021-10-17 00:29:07 +0200

    austin: some experiments around amazon austin - wip

---
+++ systems/tablet_amazon_austin/get-files.sh
@@ -0,0 +1,29 @@
+# this file is supposed to be sourced by the get-files shell script
+
+# right now i'm still using a postmarketos kernel+initrd=boot.img and will maybe later move to an own boot.img
+
+# get a kernel etc. from a special portmarketos based build
+
+amazon_austin_pmos_release_version="linux-amazon-austin-3.10.54-r3"
+
+rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
+mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
+wget -v https://github.com/hexdump0815/pmaports-amazon/releases/download/${amazon_austin_pmos_release_version}/boot.img.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot.img
+wget -v https://github.com/hexdump0815/pmaports-amazon/releases/download/${amazon_austin_pmos_release_version}/config-amazon-austin.armv7.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/config-amazon-austin.armv7
+wget -v https://github.com/hexdump0815/pmaports-amazon/releases/download/${amazon_austin_pmos_release_version}/initramfs-extra.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/initramfs-extra
+wget -v https://github.com/hexdump0815/pmaports-amazon/releases/download/${amazon_austin_pmos_release_version}/initramfs.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/initramfs
+wget -v https://github.com/hexdump0815/pmaports-amazon/releases/download/${amazon_austin_pmos_release_version}/vmlinuz.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/vmlinuz.gz
+wget -v https://github.com/hexdump0815/pmaports-amazon/releases/download/${amazon_austin_pmos_release_version}/lib-modules.tar.gz -O ${DOWNLOAD_DIR}/boot-extra-${1}/lib-modules.tar.gz
+
+# get a mali blob which surprisingly to some degree is useable for this older fire 7 tablet
+
+amazon_austin_mali_blob_version="771a635a97ef86d185614027f53c92862a4a4a9c"
+gl4es_focal_armv7l_version="9051dfe1f2198e2ed41c322359ee8324043d55a9"
+
+rm -f ${DOWNLOAD_DIR}/opengl-tablet_amazon_austin-armv7l.tar.gz
+wget -v https://github.com/hexdump0815/linux-amazon-mediatek-mt8127-kernel/raw/${amazon_austin_mali_blob_version}/misc/opt-mali-meson-r4p0-armv7l.tar.gz -O ${DOWNLOAD_DIR}/opengl-tablet_amazon_austin-armv7l.tar.gz
+rm -f ${DOWNLOAD_DIR}/opengl-fbdev-tablet_amazon_austin-armv7l.tar.gz
+wget -v https://github.com/hexdump0815/linux-amazon-mediatek-mt8127-kernel/raw/${amazon_austin_mali_blob_version}/misc/opt-mali-meson-fbdev-r4p0-armv7l.tar.gz -O ${DOWNLOAD_DIR}/opengl-fbdev-tablet_amazon_austin-armv7l.tar.gz
+
+rm -f ${DOWNLOAD_DIR}/gl4es-armv7l-focal.tar.gz
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-stable-kernel/raw/${gl4es_focal_armv7l_version}/misc/gl4es-armv7l-ubuntu.tar.gz -O ${DOWNLOAD_DIR}/gl4es-armv7l-focal.tar.gz
---
+++ systems/tablet_amazon_austin/readme.md
@@ -0,0 +1,17 @@
+# amazon fire 7 2017 tablet - austin
+
+## bootable sd card images
+
+- none yet
+
+## tested systems - working
+
+- amazon fire 7 2017 tablet - austin
+
+## kernel build notes
+
+right now i'm still using a postmarketos kernel+initrd=boot.img and will maybe later move to an own boot.img
+
+## special notes
+
+- this is not useable at all yet, still in a very early and experimental phase
