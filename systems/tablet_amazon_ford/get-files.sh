aff321ff29a072d1f53895dfc0e076c47fd8ea6f
hexdump <hexdump0815@googlemail.com>
2021-10-16 13:53:51 +0200

    ford: get mali blob

--- systems/tablet_amazon_ford/get-files.sh
+++ systems/tablet_amazon_ford/get-files.sh
@@ -19,3 +19,16 @@ wget -v https://github.com/hexdump0815/pmaports-amazon/releases/download/${amazo
 wget -v https://github.com/hexdump0815/pmaports-amazon/releases/download/${amazon_ford_pmos_release_version}/initramfs.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/initramfs
 wget -v https://github.com/hexdump0815/pmaports-amazon/releases/download/${amazon_ford_pmos_release_version}/vmlinuz.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/vmlinuz.gz
 wget -v https://github.com/hexdump0815/pmaports-amazon/releases/download/${amazon_ford_pmos_release_version}/lib-modules.tar.gz -O ${DOWNLOAD_DIR}/boot-extra-${1}/lib-modules.tar.gz
+
+# get a mali blob which surprisingly to some degree is useable for this older fire 7 tablet
+
+amazon_ford_mali_blob_version="771a635a97ef86d185614027f53c92862a4a4a9c"
+gl4es_focal_armv7l_version="9051dfe1f2198e2ed41c322359ee8324043d55a9"
+
+rm -f ${DOWNLOAD_DIR}/opengl-tablet_amazon_ford-armv7l.tar.gz
+wget -v https://github.com/hexdump0815/linux-amazon-mediatek-mt8127-kernel/raw/${tablet_amazon_ford_mali_blob_version}/misc/opt-mali-meson-r4p0-armv7l.tar.gz -O ${DOWNLOAD_DIR}/opengl-tablet_amazon_ford-armv7l.tar.gz
+rm -f ${DOWNLOAD_DIR}/opengl-fbdev-tablet_amazon_ford-armv7l.tar.gz
+wget -v https://github.com/hexdump0815/linux-amazon-mediatek-mt8127-kernel/raw/${tablet_amazon_ford_mali_blob_version}/misc/opt-mali-meson-fbdev-r4p0-armv7l.tar.gz -O ${DOWNLOAD_DIR}/opengl-fbdev-tablet_amazon_ford-armv7l.tar.gz
+
+rm -f ${DOWNLOAD_DIR}/gl4es-armv7l-focal.tar.gz
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-stable-kernel/raw/${gl4es_focal_armv7l_version}/misc/gl4es-armv7l-ubuntu.tar.gz -O ${DOWNLOAD_DIR}/gl4es-armv7l-focal.tar.gz
