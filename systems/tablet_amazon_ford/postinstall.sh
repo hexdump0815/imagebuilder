73e43b8a01b48af65cc0b8d8da8c04aa981fe8dc
hexdump <hexdump0815@googlemail.com>
2021-10-16 14:21:22 +0200

    ford,douglas: some more xorg config files

--- systems/tablet_amazon_ford/postinstall.sh
+++ systems/tablet_amazon_ford/postinstall.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 cp -v etc/X11/xorg.conf.d.samples/11-fbdev.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/31-monitor-no-dpms.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
