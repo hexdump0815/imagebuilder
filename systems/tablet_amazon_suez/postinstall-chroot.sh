1810d3813924e03ce7ea0835fca19bb70f37b90f
hexdump <hexdump0815@googlemail.com>
2021-10-28 23:25:11 +0200

    douglas,suez: remove, mask, disable some unneeded stuff

---
+++ systems/tablet_amazon_suez/postinstall-chroot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# remove and disable some things which are not needed or disturbing on this weak
+# tablet with its legacy kernel to speed up the boot process
+apt-get remove --purge exim4-base exim4-daemon-light exim4-config libgnutls-dane0 libunbound8
+systemctl disable lvm2-monitor.service lvm2-lvmpolld.socket
+systemctl mask systemd-networkd-wait-online.service
+systemctl mask NetworkManager-wait-online.service
+systemctl mask serial-getty@ttyMT0.service
+systemctl mask blueman-mechanism.service
+systemctl mask bluetooth.service
