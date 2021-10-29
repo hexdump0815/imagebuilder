771313ea92c4de1e00047b105271352ec45108f7
hexdump <hexdump0815@googlemail.com>
2021-10-29 20:37:58 +0200

    use non interactive apt-get install mode

--- systems/tablet_amazon_suez/postinstall-chroot.sh
+++ systems/tablet_amazon_suez/postinstall-chroot.sh
@@ -2,7 +2,7 @@
 
 # remove and disable some things which are not needed or disturbing on this weak
 # tablet with its legacy kernel to speed up the boot process
-apt-get remove --purge exim4-base exim4-daemon-light exim4-config libgnutls-dane0 libunbound8
+apt-get -yq remove --purge exim4-base exim4-daemon-light exim4-config libgnutls-dane0 libunbound8
 systemctl disable lvm2-monitor.service lvm2-lvmpolld.socket
 systemctl mask systemd-networkd-wait-online.service
 systemctl mask NetworkManager-wait-online.service
