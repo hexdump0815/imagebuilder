437aa4d030b0b01f280cbe82860bfc5bde73a55b
hexdump <hexdump0815@googlemail.com>
2021-10-29 21:34:33 +0200

    install debian files for bullseye

--- files/extra-files-armv7l-bullseye/scripts/install-gl4es.sh
+++ files/extra-files-armv7l-bullseye/scripts/install-gl4es.sh
@@ -4,6 +4,6 @@ cd `dirname $0`
 . versions.conf
 
 cd /
-wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-stable-kernel/raw/${gl4es_bullseye_armv7l_version}/misc/gl4es-armv7l-ubuntu.tar.gz -O /tmp/gl4es-armv7l-bullseye.tar.gz
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-stable-kernel/raw/${gl4es_bullseye_armv7l_version}/misc/gl4es-armv7l-debian.tar.gz -O /tmp/gl4es-armv7l-bullseye.tar.gz
 tar xzf /tmp/gl4es-armv7l-bullseye.tar.gz
 rm -f /tmp/gl4es-armv7l-bullseye.tar.gz
