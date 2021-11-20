71d151c8fafaf6ab6eb020cc72a44b601d0aecb2
hexdump <hexdump0815@googlemail.com>
2021-11-20 11:46:19 +0100

    gru: use newer kernel with some fixes

--- systems/chromebook_gru/get-files.sh
+++ systems/chromebook_gru/get-files.sh
@@ -1,6 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-chromebook_gru_release_version="5.15.2-stb-cbg"
+chromebook_gru_release_version="5.15.2-stb-cbg%2B"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_gru-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-stable-kernel/releases/download/${chromebook_gru_release_version}/${chromebook_gru_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_gru-${2}.tar.gz
