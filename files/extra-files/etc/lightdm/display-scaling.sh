8e85d9930bd432d0c9c2a697fc38e92f55559769
hexdump <hexdump0815@googlemail.com>
2021-11-22 07:49:00 +0100

    add some more display scaling examples

--- files/extra-files/etc/lightdm/display-scaling.sh
+++ files/extra-files/etc/lightdm/display-scaling.sh
@@ -3,6 +3,9 @@
 OUTPUT=$(xrandr | grep "connected primary" | awk '{print $1}')
 
 xrandr --output $OUTPUT --mode 1200x1920 --panning 800x1280 --scale 0.6666x0.6666
+#xrandr --output $OUTPUT --mode 1920x1080 --panning 1280x720 --scale 0.6666x0.6666
+#xrandr --output $OUTPUT --mode 1920x1080 --panning 1440x810 --scale 0.75x0.75
+#xrandr --output $OUTPUT --mode 1920x1200 --panning 1440x900 --scale 0.75x0.75
 
 # this is needed as for some reason the above command gives an error, but still does what it should
 exit 0
