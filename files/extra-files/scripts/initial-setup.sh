1ca46abc019b8157dadd186c6615a86ab53ff01d
hexdump <hexdump0815@googlemail.com>
2021-11-26 21:53:02 +0100

    add some notes here and there

--- files/extra-files/scripts/initial-setup.sh
+++ files/extra-files/scripts/initial-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# better check /etc/default/keyboard if it looks as expected after this has been run
+# on some chromebooks for instance it seems to have problems to detect the keyboard
+# at all and skips the keyboard setup completely - then just edit the above file by
+# hand
 dpkg-reconfigure keyboard-configuration
 if [ ! -f /etc/debian_version ]; then
   # activate new setting on the console if virtual console is used
