291c9456eef281b0033f4d6b3362b6be465945de
hexdump0815 <hexdump0815@googlemail.com>
2021-11-27 09:44:48 +0100

    h6: adjust repo path to current value

--- systems/allwinner_h6/get-files.sh
+++ systems/allwinner_h6/get-files.sh
@@ -5,10 +5,10 @@ allwinner_h6_uboot_version="200718-01"
 mesa_release_version="21.0.1"
 
 rm -f ${DOWNLOAD_DIR}/kernel-allwinner_h6-${2}.tar.gz
-wget -v https://github.com/hexdump0815/linux-mainline-and-mali-allwinner-h3-h6-kernel/releases/download/${allwinner_h6_release_version}/${allwinner_h6_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-allwinner_h6-${2}.tar.gz
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-allwinner-h6-kernel/releases/download/${allwinner_h6_release_version}/${allwinner_h6_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-allwinner_h6-${2}.tar.gz
 
 #rm -f ${DOWNLOAD_DIR}/kernel-mali-allwinner_h6-${2}.tar.gz
-#wget -v https://github.com/hexdump0815/linux-mainline-and-mali-allwinner-h3-h6-kernel/releases/download/${allwinner_h6_release_version}/${allwinner_h6_release_version}-mali-h6.tar.gz -O ${DOWNLOAD_DIR}/kernel-mali-allwinner_h6-${2}.tar.gz
+#wget -v https://github.com/hexdump0815/linux-mainline-and-mali-allwinner-h6-kernel/releases/download/${allwinner_h6_release_version}/${allwinner_h6_release_version}-mali-h6.tar.gz -O ${DOWNLOAD_DIR}/kernel-mali-allwinner_h6-${2}.tar.gz
 
 rm -f ${DOWNLOAD_DIR}/boot-allwinner_h6-${2}.dd
 wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${allwinner_h6_uboot_version}/boot-allwinner_h6-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-allwinner_h6-${2}.dd
