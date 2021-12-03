9150b078787bdb297cfb54fd6948e90017fc4040
hexdump <hexdump0815@googlemail.com>
2021-12-03 21:28:15 +0100

    sd835: use newer kernel - v5.15.0 based on op5 tree

--- systems/snapdragon_835/get-files.sh
+++ systems/snapdragon_835/get-files.sh
@@ -1,6 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-snapdragon_835_release_version="5.11.14-op5-qcn"
+snapdragon_835_release_version="5.15.0-op5-qcn"
 
 rm -f ${DOWNLOAD_DIR}/kernel-snapdragon_835-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-qcom-msm8998-kernel/releases/download/${snapdragon_835_release_version}/${snapdragon_835_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-snapdragon_835-${2}.tar.gz
