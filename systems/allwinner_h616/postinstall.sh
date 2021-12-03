0dc3316b5c1de91d7d7e7cb2f3d70ea0989ce41d
hexdump <hexdump0815@googlemail.com>
2021-12-03 10:27:19 +0100

    h616: add input config for new xorg with old input
    this is a hack to make a newly compiled xorg server work somehow
    with the old input driver modules for the usb keyboard and mouse

--- systems/allwinner_h616/postinstall.sh
+++ systems/allwinner_h616/postinstall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cp -v etc/X11/xorg.conf.d.samples/11-modesetting-no-glamor.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/51-newer-xorg-old-input.conf etc/X11/xorg.conf.d
