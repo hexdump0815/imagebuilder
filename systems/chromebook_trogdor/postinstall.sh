74771b50f6965de0c38c9b8dd8614b4181f394d8
hexdump <hexdump0815@googlemail.com>
2021-12-12 21:17:54 +0100

    trogdor: minor adjustments for rmtfs

--- systems/chromebook_trogdor/postinstall.sh
+++ systems/chromebook_trogdor/postinstall.sh
@@ -4,4 +4,5 @@ cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/31-monitor-no-dpms.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
 
+# install the qcom tools qrtr-ns and rmtfs
 tar xzf postinstall/qcom-tools-${2}-${3}.tar.gz
