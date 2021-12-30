b9f418f031f4e0282ba239cd2c8be1bac1a22757
hexdump0815 <hexdump0815@googlemail.com>
2021-12-30 18:29:04 +0100

    gt510,harpia: add lost command ...

--- systems/phone_motorola_harpia/postinstall.sh
+++ systems/phone_motorola_harpia/postinstall.sh
@@ -3,6 +3,7 @@
 cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
 
 # install the kernel, initramfs, boot.img etc.
+tar xzf postinstall/boot-and-modules.tar.gz
 
 # install the qcom tools qrtr-ns and rmtfs
 tar xzf postinstall/qcom-tools-${2}-${3}.tar.gz
