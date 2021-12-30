81fe4414f6a6e1816c8b8d48e4cb90bdb0a45cce
hexdump0815 <hexdump0815@googlemail.com>
2021-12-30 10:13:10 +0100

    h6: newer kernel and u-boot

--- systems/allwinner_h6/get-files.sh
+++ systems/allwinner_h6/get-files.sh
@@ -1,17 +1,22 @@
 # this file is supposed to be sourced by the get-files shell script  
 
-allwinner_h6_release_version="5.10.25-stb-ah6%2B"
-allwinner_h6_uboot_version="200718-01"
+
+allwinner_h6_release_version="5.15.11-stb-ah6%2B"
+allwinner_h6_uboot_version="211229-01"
 mesa_release_version="21.0.1"
 
 rm -f ${DOWNLOAD_DIR}/kernel-allwinner_h6-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-allwinner-h6-kernel/releases/download/${allwinner_h6_release_version}/${allwinner_h6_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-allwinner_h6-${2}.tar.gz
 
-#rm -f ${DOWNLOAD_DIR}/kernel-mali-allwinner_h6-${2}.tar.gz
-#wget -v https://github.com/hexdump0815/linux-mainline-and-mali-allwinner-h6-kernel/releases/download/${allwinner_h6_release_version}/${allwinner_h6_release_version}-mali-h6.tar.gz -O ${DOWNLOAD_DIR}/kernel-mali-allwinner_h6-${2}.tar.gz
+# get different u-boot versions for different allwinner h6 systems to have them around
+rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
+mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
+for system in eachlink_h6_mini orange_pi_3 pine-h64 noname_h6_tv_box orangepi_one_plus tanix_tx6; do
+  wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${allwinner_h6_uboot_version}/boot-${system}-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-${system}.dd
+done
 
-rm -f ${DOWNLOAD_DIR}/boot-allwinner_h6-${2}.dd
-wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${allwinner_h6_uboot_version}/boot-allwinner_h6-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-allwinner_h6-${2}.dd
+# use the eachlink h6 mini u-boot as default
+cp ${DOWNLOAD_DIR}/boot-extra-${1}/boot-eachlink_h6_mini.dd ${DOWNLOAD_DIR}/boot-allwinner_h6-${2}.dd
 
 # get the self built fresher mesa
 wget https://github.com/hexdump0815/mesa-etc-build/releases/download/${mesa_release_version}/opt-mesa-${mesa_release_version}-${3}-${2}.tar.gz -O ${DOWNLOAD_DIR}/opt-mesa-${3}-${2}.tar.gz
