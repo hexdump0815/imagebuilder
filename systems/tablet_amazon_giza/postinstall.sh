a81670863442585d5e047b66a8c73f92ec9c11db
hexdump <hexdump0815@googlemail.com>
2022-02-19 19:30:35 +0100

    giza,karnak: enable monitor powermgmt etc. - wip

--- systems/tablet_amazon_giza/postinstall.sh
+++ systems/tablet_amazon_giza/postinstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cp -v etc/X11/xorg.conf.d.samples/11-fbdev-rotate-right.conf etc/X11/xorg.conf.d
-cp -v etc/X11/xorg.conf.d.samples/31-monitor-no-dpms.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/31-monitor.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
+
+cp -v etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
 
 tar xzf postinstall/tablet_amazon_douglas-firmware.tar.gz
--- systems/tablet_amazon_karnak/postinstall.sh
+++ systems/tablet_amazon_karnak/postinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cp -v etc/X11/xorg.conf.d.samples/11-fbdev-rotate-right.conf etc/X11/xorg.conf.d
-cp -v etc/X11/xorg.conf.d.samples/31-monitor-no-dpms.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/31-monitor.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
+
+cp -v etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
