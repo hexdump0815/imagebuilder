ed5af0b5d2cc5af7786d1161d8765f094a7ebdce
hexdump <hexdump0815@googlemail.com>
2022-02-23 20:12:14 +0100

    ford,austin,giza,douglas,karnak,suez: remove unneeded monitor cfg

--- systems/tablet_amazon_austin/postinstall.sh
+++ systems/tablet_amazon_austin/postinstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cp -v etc/X11/xorg.conf.d.samples/11-fbdev-rotate-right.conf etc/X11/xorg.conf.d
-cp -v etc/X11/xorg.conf.d.samples/31-monitor-no-dpms.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
 
 # bullseye
--- systems/tablet_amazon_suez/postinstall.sh
+++ systems/tablet_amazon_suez/postinstall.sh
@@ -6,7 +6,6 @@ echo "load-module module-alsa-sink device=sysdefault" >> etc/pulse/default.pa
 echo "#load-module module-alsa-source device=sysdefault" >> etc/pulse/default.pa
 
 cp -v etc/X11/xorg.conf.d.samples/11-fbdev-rotate-right.conf etc/X11/xorg.conf.d
-cp -v etc/X11/xorg.conf.d.samples/31-monitor-no-dpms.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
 
 # bullseye
