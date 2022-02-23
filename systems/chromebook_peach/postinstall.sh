b2ef5cb837c9721c91c3c4909272ed07c6420e30
hexdump <hexdump0815@googlemail.com>
2022-02-23 20:11:02 +0100

    snow,peach,gt510: add (disabled) display pwrmgmt

--- systems/chromebook_peach/postinstall.sh
+++ systems/chromebook_peach/postinstall.sh
@@ -8,3 +8,13 @@
 
 cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
+
+cp -v etc/X11/xorg.conf.d.samples/31-monitor-no-dpms.conf etc/X11/xorg.conf.d
+# this is not set by default, as there were some drm unblank kernel errors
+## bullseye
+#if [ -f etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled ]; then
+#  cp -v etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+## focal
+#elif [ -f etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled ]; then
+#  cp -v etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+#fi
--- systems/tablet_samsung_gt510/postinstall.sh
+++ systems/tablet_samsung_gt510/postinstall.sh
@@ -4,7 +4,17 @@ cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
 # in case display should be rotated - in this case also uncomment
 # the lines in /etc/udev/rules.d/90-android-touch-dev.rules and adjust
 # /scripts/fix-display.sh
-#cp -v etc/X11/xorg.conf.d.samples/31-monitor-no-dpms-rotate-right.conf etc/X11/xorg.conf.d
+#cp -v etc/X11/xorg.conf.d.samples/31-monitor-rotate-right.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
+
+# this has not yet been tested, but most probably it will work
+## bullseye
+#if [ -f etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled ]; then
+#  cp -v etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+## focal
+#elif [ -f etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled ]; then
+#  cp -v etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+#fi
 
 # install the kernel, initramfs, boot.img etc.
 tar xzf postinstall/boot-and-modules.tar.gz
