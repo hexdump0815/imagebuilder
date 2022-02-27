8dcea1eafae7b83e9b2a704b2034b39b62f552ed
hexdump <hexdump0815@googlemail.com>
2022-02-27 08:56:10 +0100

    files: add install script for latest stable xpra

---
+++ files/extra-files-bullseye/scripts/install-xpra.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install the official latest stable xpra version as the version in bullseye it too old
+
+wget -qO - https://xpra.org/gpg.asc | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/xpra.gpg
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/xpra.gpg] https://xpra.org/ bullseye main' | sudo tee /etc/apt/sources.list.d/xpra.list
+apt-get update
+apt-get -y install xpra
