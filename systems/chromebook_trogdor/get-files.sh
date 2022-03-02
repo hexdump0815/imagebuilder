43e860f9a04e3762748abac68dc1132b39e1548e
hexdump <hexdump0815@googlemail.com>
2022-03-02 19:53:55 +0100

    trogdor: kernel v5.15.22

--- systems/chromebook_trogdor/get-files.sh
+++ systems/chromebook_trogdor/get-files.sh
@@ -1,6 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-chromebook_trogdor_release_version="5.15.2-stb-cbq"
+chromebook_trogdor_release_version="5.15.22-stb-cbq"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_trogdor-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-generic-stable-kernel/releases/download/${chromebook_trogdor_release_version}/${chromebook_trogdor_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_trogdor-${2}.tar.gz
