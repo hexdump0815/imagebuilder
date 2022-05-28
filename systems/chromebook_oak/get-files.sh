25c94825529d9336db3d9601f6878ef2210d1bab
hexdump <hexdump0815@googlemail.com>
2022-05-28 16:46:16 +0200

    oak: switch kernel to v5.18.0 with mglru patches
    dpms display suspend/resume is not working properly with v5.18, so
    disable it in the power manager settings

--- systems/chromebook_oak/get-files.sh
+++ systems/chromebook_oak/get-files.sh
@@ -1,6 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-chromebook_oak_release_version="5.10.99-stb-mt7%2B"
+chromebook_oak_release_version="5.18.0-stb-mt7%2B"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_oak-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/${chromebook_oak_release_version}/${chromebook_oak_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_oak-${2}.tar.gz
