94c0c7c2942b89b641c33e6976c53767ed31c359
hexdump <hexdump0815@googlemail.com>
2022-05-28 21:33:05 +0200

    nyan: switch kernel to v5.18.0 with mglru patches

--- systems/chromebook_nyan/get-files.sh
+++ systems/chromebook_nyan/get-files.sh
@@ -6,8 +6,8 @@ LEGACY_KERNEL="no"
 # this one is for the v5.4 kernel tree which had a way more reliable display handling in the past
 #chromebook_nyan_release_version="5.4.125-stb-cbt%2B"
 #chromebook_nyan_kernel_tree="linux-mainline-tegra-k1-kernel"
-# this one is for the v5.15 newer kernel which seems to work similarly well now
-chromebook_nyan_release_version="5.15.22-stb-cbt%2B"
+# this one is for the v5.18 newer kernel which seems to work similarly well now
+chromebook_nyan_release_version="5.18.0-stb-cbt%2B"
 chromebook_nyan_kernel_tree="linux-mainline-and-mali-generic-stable-kernel"
 chromebook_nyan_legacy_release_version="3.10.18-cos-r91"
 chromebook_nyan_2g_uboot_version="v2021.10-cbt"
