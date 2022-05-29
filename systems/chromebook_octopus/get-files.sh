5efeff9112c4212c7b82ad52e03c134b7fc40612
hexdump <hexdump0815@googlemail.com>
2022-05-29 10:59:39 +0200

    octopus: put the coral kernel to /boot/extre just in case

--- systems/chromebook_octopus/get-files.sh
+++ systems/chromebook_octopus/get-files.sh
@@ -1,6 +1,7 @@
 # this file is supposed to be sourced by the get-files shell script
 
 chromebook_octopus_release_version="5.18.0-stb-cbo%2B"
+chromebook_coral_release_version="5.18.0-stb-cbc%2B"
 chromebook_octopus_legacy_release_version="4.14.228-cos-r91+"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_octopus-${2}.tar.gz
@@ -8,6 +9,8 @@ wget -v https://github.com/hexdump0815/linux-mainline-x86-64-kernel/releases/dow
 
 ( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_octopus-${2}.tar.gz boot ; mv boot/vmlinux.kpart-* boot-chromebook_octopus-${2}.dd ; rm -rf boot )
 
+# put some alternative kernels to /boot/extra
 rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
 mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
+wget -v https://github.com/hexdump0815/linux-mainline-x86-64-kernel/releases/download/${chromebook_coral_release_version}/${chromebook_coral_release_version}.tar.gz -O ${DOWNLOAD_DIR}/boot-extra-${1}/kernel-chromebook_coral-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-chromeos-kernel/releases/download/${chromebook_octopus_legacy_release_version}/${chromebook_octopus_legacy_release_version}.tar.gz -O ${DOWNLOAD_DIR}/boot-extra-${1}/kernel-chromebook_octopus-legacy.tar.gz
