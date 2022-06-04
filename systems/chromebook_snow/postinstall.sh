9571506be3df83c63865ab259a60803c816e3220
hexdump <hexdump0815@googlemail.com>
2022-06-04 21:26:20 +0200

    snow: install normal xorg monitor.conf and handle dpms issues
    the dpms issues are handled via adjusted power manager config now

--- systems/chromebook_snow/postinstall.sh
+++ systems/chromebook_snow/postinstall.sh
@@ -7,13 +7,13 @@ echo "#load-module module-alsa-source device=sysdefault" >> etc/pulse/default.pa
 
 cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/31-monitor.conf etc/X11/xorg.conf.d
 
-cp -v etc/X11/xorg.conf.d.samples/31-monitor-no-dpms.conf etc/X11/xorg.conf.d
-# this is not set by default, as there were some drm unblank kernel errors
-## bullseye
-#if [ -f etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled ]; then
-#  cp -v etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
-## focal and jammy
-#elif [ -f etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled ]; then
-#  cp -v etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
-#fi
+# lets better use the dpms suspend free version here, as there were some drm unblank kernel errors
+# bullseye
+if [ -f etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled-no-dpms-suspend ]; then
+  cp -v etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled-no-dpms-suspend etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+# focal and jammy
+elif [ -f etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled-no-dpms-suspend ]; then
+  cp -v etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled-no-dpms-suspend etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+fi
