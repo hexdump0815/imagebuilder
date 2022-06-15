965525385f846cfe17d3d7a7a1884cc3f8bf156f
hexdump <hexdump0815@googlemail.com>
2022-06-15 22:01:38 +0200

    rk33xx: use proper rk3328 boot block for default booted system
    by default the image will boot with a rk3328 tv box dtb with
    increased cpu voltage - write a boot block matching this default
    system better (tested with a rk3328 t9 tv box but should work
    for some other rk3328 tv boxes as well maybe)

--- systems/rockchip_rk33xx/get-files.sh
+++ systems/rockchip_rk33xx/get-files.sh
@@ -17,14 +17,13 @@ wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-k
 #wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/releases/download/${rockchip_rk33xx_release_version}/${rockchip_rk33xx_release_version}-mali-rk3399.tar.gz -O ${DOWNLOAD_DIR}/kernel-mali-b-alt-rockchip_rk33xx-${2}.tar.gz
 
 rm -f ${DOWNLOAD_DIR}/boot-rockchip_rk33xx-${2}.dd
-wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${rockchip_rk33xx_uboot_rk3318_ddrbin_legacy_atf_version}/boot-rk3318-ddrbin-legacy-atf-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-rockchip_rk33xx-${2}.dd
-
-# get different u-boot versions for different amlogic versions to have them around
+wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${rockchip_rk33xx_uboot_ddrbin_spl_version}/boot-rk3328-ddrbin-spl-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-rockchip_rk33xx-${2}.dd
+# get different u-boot versions for different rk33xx rockchip versions to have them around
 rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
 mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
-cp ${DOWNLOAD_DIR}/boot-rockchip_rk33xx-${2}.dd ${DOWNLOAD_DIR}/boot-extra-${1}/boot-rk3318-ddrbin-legacy-atf.dd
+cp ${DOWNLOAD_DIR}/boot-rockchip_rk33xx-${2}.dd ${DOWNLOAD_DIR}/boot-extra-${1}/boot-rk3328-ddrbin-spl.dd
+wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${rockchip_rk33xx_uboot_rk3318_ddrbin_legacy_atf_version}/boot-rk3318-ddrbin-legacy-atf-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-rk3318-ddrbin-legacy-atf-aarch64.dd.gz
 wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${rockchip_rk33xx_uboot_rk3328_ddrbin_legacy_atf_version}/boot-rk3328-ddrbin-legacy-atf-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-rk3328-ddrbin-legacy-atf.dd
-wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${rockchip_rk33xx_uboot_ddrbin_spl_version}/boot-rk3328-ddrbin-spl-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-rk3328-ddrbin-spl.dd
 wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${rockchip_rk33xx_uboot_ddrbin_666mhz_spl_version}/boot-rk3328-ddrbin-666mhz-spl-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-rk3328-ddrbin-666mhz-spl.dd
 wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${rockchip_rk33xx_uboot_tpl_spl_version}/boot-rk3328-tpl-spl-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-rk3328-tpl-spl.dd
 wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${rockchip_rk33xx_uboot_tpl_spl_version}/u-boot-rk3328.img.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/u-boot-rk3328.img
