126640691dbf12d31a13f76d606af1aacf0f5d35
hexdump <hexdump0815@googlemail.com>
2022-06-16 20:22:44 +0200

    h6: looks like gpu accel was broken on focal already

--- systems/allwinner_h6/postinstall.sh
+++ systems/allwinner_h6/postinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# for some strange reason mesa is broken on jammy - both the shipped 22.0 as well
+# for some strange reason mesa is broken on ubuntu - both the shipped 22.0 as well
 # as the self built 22.1 is so full of display errors, that it is unuseable ...
 # bullseye works quite well with the same versions - so disable gpu accel in this
 # case - hopefully later versions of mesa will fix this
-if [ "${3}" = "jammy" ]; then
+if [ "${3}" = "focal" ] || [ "${3}" = "jammy" ]; then
   cp -v etc/X11/xorg.conf.d.samples/11-modesetting-no-glamor.conf etc/X11/xorg.conf.d
 else
   cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
