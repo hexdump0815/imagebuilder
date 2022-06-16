eb9e6bc4332dda42c074fca22f905466d185a4f5
hexdump <hexdump0815@googlemail.com>
2022-06-16 19:29:03 +0200

    h6: disable mesa gpu accel on jammy as its not really useable

--- systems/allwinner_h6/postinstall.sh
+++ systems/allwinner_h6/postinstall.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
+# for some strange reason mesa is broken on jammy - both the shipped 22.0 as well
+# as the self built 22.1 is so full of display errors, that it is unuseable ...
+# bullseye works quite well with the same versions - so disable gpu accel in this
+# case - hopefully later versions of mesa will fix this
+if [ "${3}" = "jammy" ]; then
+  cp -v etc/X11/xorg.conf.d.samples/11-modesetting-no-glamor.conf etc/X11/xorg.conf.d
+else
+  cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
+fi
 cp -v etc/X11/xorg.conf.d.samples/31-monitor-no-dpms.conf etc/X11/xorg.conf.d
