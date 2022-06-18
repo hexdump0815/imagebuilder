6c8cf0747f0f3fdc98d9f932101e9ce606628a76
hexdump <hexdump0815@googlemail.com>
2022-06-18 08:17:54 +0200

    amlogic gx: add lima xorg config bydefault
    the default system for this image is the odroid c2 and for xorg to
    work on it this config is needed ... it is also required for the
    other mali 450 soc (s905, s905x, s905w) based devices and the good
    thing is that it seems to work fine still for the mali g31 based
    systems as well which do not really need this extra config (tested
    on sm1)

--- systems/amlogic_gx/postinstall.sh
+++ systems/amlogic_gx/postinstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/13-lima-meson.conf etc/X11/xorg.conf.d
 
 cp -v boot/extra/gxl-tv-box-u-boot.bin boot/u-boot.bin
