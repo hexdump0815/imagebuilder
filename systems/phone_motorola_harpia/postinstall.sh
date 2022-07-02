6c63b3bc84b05282f3e5866d6df99ba1a572e1b0
hexdump <hexdump0815@googlemail.com>
2022-07-02 14:14:47 +0200

    harpia: landscape mode notes and display pm off
    - add notes about how to rotate the screen to landscape mode
    - disable display power management as it seems to be broken here

--- systems/phone_motorola_harpia/postinstall.sh
+++ systems/phone_motorola_harpia/postinstall.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
+# in case display should be rotated - in this case also uncomment
+# the lines in /etc/udev/rules.d/90-android-touch-dev.rules
+#cp -v etc/X11/xorg.conf.d.samples/31-monitor-rotate-right.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
+
+# bullseye
+if [ -f etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-off ]; then
+  cp -v etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-off etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+# focal and jammy
+elif [ -f etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-off ]; then
+  cp -v etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-off etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+fi
 
 # install the kernel, initramfs, boot.img etc.
 tar xzf postinstall/boot-and-modules.tar.gz
