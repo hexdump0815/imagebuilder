392ec566b4b6e6ec8c11666ad1a093addfe04741
hexdump <hexdump0815@googlemail.com>
2022-08-14 10:43:36 +0200

    trogdor: move to v5.19.1 kernel

--- systems/chromebook_trogdor/get-files.sh
+++ systems/chromebook_trogdor/get-files.sh
@@ -1,6 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-chromebook_trogdor_release_version="5.18.1-stb-cbq%2B"
+chromebook_trogdor_release_version="5.19.1-stb-cbq%2B"
 mesa_release_version="22.1.1"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_trogdor-${2}.tar.gz
