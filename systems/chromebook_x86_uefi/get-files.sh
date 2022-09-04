3c69184d968d0952995d21c54008c742983ea166
hexdump <hexdump0815@googlemail.com>
2022-09-04 10:56:02 +0200

    x86_uefi: add newer self built debian kernel as extra

--- systems/chromebook_x86_uefi/get-files.sh
+++ systems/chromebook_x86_uefi/get-files.sh
@@ -1,3 +1,8 @@
 # this file is supposed to be sourced by the get-files shell script
 
-echo "no files to get here yet"
+chromebook_x86_uefi_extra_release_version="5.19.6-stb-odk+"
+
+# put the newer extra kernel into /boot/extra to have it around
+rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
+mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
+wget -v https://github.com/hexdump0815/linux-mainline-x86-64-kernel/releases/download/${chromebook_x86_uefi_extra_release_version}/${chromebook_x86_uefi_extra_release_version}.tar.gz -O ${DOWNLOAD_DIR}/boot-extra-${1}/${chromebook_x86_uefi_extra_release_version}.tar.gz
