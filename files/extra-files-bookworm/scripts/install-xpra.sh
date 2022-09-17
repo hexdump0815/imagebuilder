4cc27b3bc254b229752848784890ed2e90577564
hexdump <hexdump0815@googlemail.com>
2022-09-17 10:31:35 +0200

    files: bookworm requires the latest xpra beta to work
    the regular latest vrsion tries to install packages, which do not seem
    to exist in bookworm - the latest xpra beta version works though, so
    switch to it instead for bookworm

--- files/extra-files-bookworm/scripts/install-xpra.sh
+++ files/extra-files-bookworm/scripts/install-xpra.sh
@@ -4,5 +4,6 @@
 
 wget -qO - https://xpra.org/gpg.asc | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/xpra.gpg
 echo 'deb [signed-by=/etc/apt/trusted.gpg.d/xpra.gpg] https://xpra.org/ bookworm main' | sudo tee /etc/apt/sources.list.d/xpra.list
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/xpra.gpg] https://xpra.org/beta/ bookworm main' | sudo tee /etc/apt/sources.list.d/xpra.list
 apt-get update
 apt-get -y install xpra
