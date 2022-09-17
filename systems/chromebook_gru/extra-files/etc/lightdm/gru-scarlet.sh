2488a5d4e9351711a42f3f9b9b927cbc224033f1
hexdump <hexdump0815@googlemail.com>
2022-09-17 10:29:14 +0200

    gru: scarlet requires some extra touch screen config
    all this is wip: a sample display rotation and scaling entry for scarlet
    and the touch screen config required for it - most probably some simple
    touch screen config might even be required in non rotated mode - to be
    continued ...

---
+++ systems/chromebook_gru/extra-files/etc/lightdm/gru-scarlet.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# landscape mode and scale down thedisplay to 1280x960
+xrandr --output DSI-1 --mode 1536x2048 --panning 960x1280 --scale 0.625x0.625 --rotate left
+
+# find the touch screen input
+TOUCH_ID=`xinput list | grep "Elan Touchscreen" | grep pointer | sed 's,.*id=,,g' | awk '{print $1}'`
+# find the pen input
+PEN_ID=`xinput list | grep "Stylus Pen" | grep pointer | sed 's,.*id=,,g' | awk '{print $1}'`
+
+# see https://linuxhint.com/rotate-screen-in-raspberry-pi/ for possible transformations
+# do not ask where the 0.75 is coming from, but this value scales the y axis properly
+xinput set-prop $TOUCH_ID "Coordinate Transformation Matrix" 0 -1 1 0.75 0 0 0 0 1
+xinput set-prop $PEN_ID "Coordinate Transformation Matrix" 0 -1 1 0.75 0 0 0 0 1
+
+exit 0
