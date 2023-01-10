0841e49a5720f62aa5cadfd9293380742d81a57b
hexdump <hexdump0815@googlemail.com>
2023-01-10 21:59:40 +0100

    a3u,x86_uefi,x86_mbr: do not install extra kernel to save some space

--- systems/atom_x86_with_32bit_uefi/get-files.sh
+++ systems/atom_x86_with_32bit_uefi/get-files.sh
@@ -1,8 +1,10 @@
 # this file is supposed to be sourced by the get-files shell script
 
-chromebook_x86_uefi_extra_release_version="6.1.0-stb-odk%2B"
+echo "no files to get here yet"
 
-# put the newer extra kernel into /boot/extra to have it around
-rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
-mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
-wget -v https://github.com/hexdump0815/linux-mainline-x86-64-kernel/releases/download/${chromebook_x86_uefi_extra_release_version}/${chromebook_x86_uefi_extra_release_version}.tar.gz -O ${DOWNLOAD_DIR}/boot-extra-${1}/${chromebook_x86_uefi_extra_release_version}.tar.gz
+#chromebook_x86_uefi_extra_release_version="6.1.0-stb-odk%2B"
+#
+## put the newer extra kernel into /boot/extra to have it around
+#rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
+#mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
+#wget -v https://github.com/hexdump0815/linux-mainline-x86-64-kernel/releases/download/${chromebook_x86_uefi_extra_release_version}/${chromebook_x86_uefi_extra_release_version}.tar.gz -O ${DOWNLOAD_DIR}/boot-extra-${1}/${chromebook_x86_uefi_extra_release_version}.tar.gz
