377fed01192d8ba5cfdac08201577278185fdaf7
hexdump <hexdump0815@googlemail.com>
2023-01-22 17:21:17 +0100

    rk356x: add experimental rk356x support - wip
    this is supposed to be an image useable for rockchip rk3566 and rk3568
    based devices - so far it only has been tested to work with a x96 x6 tv
    box with a rk3566 and that one seems to work except ethernet - even the
    panfrost gpu support seems to work (although not that fast in xorg)
    
    there are bootblocks from minimyth2 included in /boot/extra for the
    pine64 quartz64 b and the radxa rock 3a+b - no idea if it will be
    working well with those

---
+++ systems/rockchip_rk356x/get-files.sh
@@ -0,0 +1,18 @@
+# this file is supposed to be sourced by the get-files shell script
+
+rockchip_rk356x_release_version="6.1.6-stb-r56%2B"
+rockchip_rk356x_u_boot_binary_version="ac8666b89e8d5787ad31c5ed22a1c51865c0f43f"
+
+rm -f ${DOWNLOAD_DIR}/kernel-rockchip_rk356x-${2}.tar.gz
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/releases/download/${rockchip_rk356x_release_version}/${rockchip_rk356x_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-rockchip_rk356x-${2}.tar.gz
+
+rm -f ${DOWNLOAD_DIR}/boot-rockchip_rk356x-${2}.dd
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-x96_x6.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-rockchip_rk356x-${2}.dd
+
+# get different u-boot versions for different rk33xx rockchip versions to have them around
+rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
+mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
+cp ${DOWNLOAD_DIR}/boot-rockchip_rk356x-${2}.dd ${DOWNLOAD_DIR}/boot-extra-${1}/boot-x96_x6.dd
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-quartz64-b.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-quartz64-b.dd
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-rock3-a.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-rock3-a.dd
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-rock3-c.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-rock3-c.dd
---
+++ systems/rockchip_rk356x/readme.md
@@ -0,0 +1,37 @@
+# rockchip rk3566, rk3568
+
+## bootable sd card images
+
+- none yet
+
+## tested systems - working
+
+- x96 x6 tv box
+
+## untested systems
+
+- pine64 quartz64 b
+- radxa rock 3a
+- radxa rock 3b
+
+some more work will be required to make the devices below working
+
+- other rockchip rk3566 tv boxes
+- other rk3566 or rk3568 sbc devices
+
+## kernel build notes
+
+- https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/blob/master/readme.r56
+
+## u-boot build notes
+
+- none yet as for now the minimyth2 boot blocks are used
+
+## priority
+
+- medium: some first experiments with rk356x devices
+
+## special notes
+
+- all this is very much wip and not that useable yet
+- more info coming soon
