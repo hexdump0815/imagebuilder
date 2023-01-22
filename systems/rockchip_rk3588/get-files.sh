26b5e796fb6606b206c680a611a4ca22b76fb702
hexdump <hexdump0815@googlemail.com>
2023-01-22 22:36:38 +0100

    rk3588: add experimental rk3588 support - wip
    this is supposed to be an image useable for rockchip rk3588 based devices
    so far it only has been tested to work with an orange pi 5 sbc
    
    this image is currently based on the legacy rockchip v5.10 kernel (the
    kernel tree from radxa is currently used as base - this and the patches
    are taken over from armbian for now) as mainline is just in the very
    earliest stages for rk3588
    
    there are bootblocks from armbian included in /boot/extra for the radxa
    rock 5b as well - no idea if it will be working well with those

---
+++ systems/rockchip_rk3588/get-files.sh
@@ -0,0 +1,16 @@
+# this file is supposed to be sourced by the get-files shell script
+
+rockchip_rk3588_release_version="5.10.110-rxa-r58%2B"
+rockchip_rk3588_u_boot_binary_version="b9f18fe55c4f6663bb944343abdef57041890430"
+
+rm -f ${DOWNLOAD_DIR}/kernel-rockchip_rk3588-${2}.tar.gz
+wget -v https://github.com/hexdump0815/linux-rockchip-rk3588-kernel/releases/download/${rockchip_rk3588_release_version}/${rockchip_rk3588_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-rockchip_rk3588-${2}.tar.gz
+
+rm -f ${DOWNLOAD_DIR}/boot-rockchip_rk3588-${2}.dd
+wget -v https://github.com/hexdump0815/linux-rockchip-rk3588-kernel/raw/${rockchip_rk3588_u_boot_binary_version}/misc.r58/u-boot/boot-orange-pi-5.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-rockchip_rk3588-${2}.dd
+
+# get different u-boot versions for different rk33xx rockchip versions to have them around
+rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
+mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
+cp ${DOWNLOAD_DIR}/boot-rockchip_rk3588-${2}.dd ${DOWNLOAD_DIR}/boot-extra-${1}/boot-orange-pi-5.dd
+wget -v https://github.com/hexdump0815/linux-rockchip-rk3588-kernel/raw/${rockchip_rk3588_u_boot_binary_version}/misc.r58/u-boot/boot-rock-5b.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-rock-5b.dd
---
+++ systems/rockchip_rk3588/readme.md
@@ -0,0 +1,34 @@
+# rockchip rk3588
+
+## bootable sd card images
+
+- none yet
+
+## tested systems - working
+
+- orange pi 5
+
+## untested systems
+
+- radxa rock 5b
+
+some more work will be required to make the devices below working
+
+- other rk3588 sbc devices
+
+## kernel build notes
+
+- https://github.com/hexdump0815/linux-rockchip-rk3588-kernel/blob/main/readme.r58
+
+## u-boot build notes
+
+- none yet as for now the legacy orange-pi-5 and rock-5b boot blocks are used
+
+## priority
+
+- medium: some first experiments with rk3588 devices
+
+## special notes
+
+- all this is very much wip and not that useable yet
+- more info coming soon
