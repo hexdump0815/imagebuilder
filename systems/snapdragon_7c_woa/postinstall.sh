c3f2540f99c18fb4d3543664809587e29a0543a6
hexdump <hexdump0815@googlemail.com>
2023-02-21 19:21:39 +0100

    sdm835,sc7-woa: install the qcom soc specific userland programs

--- systems/snapdragon_7c_woa/postinstall.sh
+++ systems/snapdragon_7c_woa/postinstall.sh
@@ -2,3 +2,6 @@
 
 cp -v etc/X11/xorg.conf.d.samples/11-fbdev.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
+
+# install the qcom tools qrtr-ns and rmtfs
+tar xzf postinstall/qcom-tools-${2}-${3}.tar.gz
