9a65826aa4aaab8b1cf11868321d8e1edf810d53
hexdump <hexdump0815@googlemail.com>
2023-03-04 19:28:56 +0100

    7c-woa: use the new v6.1.13 kernel

--- systems/snapdragon_7c_woa/get-files.sh
+++ systems/snapdragon_7c_woa/get-files.sh
@@ -1,6 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-snapdragon_7c_woa_release_version="6.1.0-rc1-stb-qc7+"
+snapdragon_7c_woa_release_version="6.1.13-stb-qc7+"
 
 rm -f ${DOWNLOAD_DIR}/kernel-snapdragon_7c_woa-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-qcom-msm8998-kernel/releases/download/${snapdragon_7c_woa_release_version}/${snapdragon_7c_woa_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-snapdragon_7c_woa-${2}.tar.gz
