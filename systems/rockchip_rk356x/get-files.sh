d778a7a1a7ce287424b6fe49395d909eccc8a882
hexdump <hexdump0815@googlemail.com>
2023-03-19 19:26:29 +0100

    rk356x,rk3588: remote gpt partition info from u-boot
    take care of what commit 3fec972a301db7246dd078776c2a2bfb3864c0dc
    removed from the create-image.sh script and put it into the get-files.sh
    scripts of the relevant systems - simply zero the first 34 512 byte
    blocks from the downloaded u-boot binaries which sometimes contain old
    gpt partition information from their old images which we do not want
    here as this can confuse the newly built images ...

--- systems/rockchip_rk356x/get-files.sh
+++ systems/rockchip_rk356x/get-files.sh
@@ -7,12 +7,12 @@ rm -f ${DOWNLOAD_DIR}/kernel-rockchip_rk356x-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/releases/download/${rockchip_rk356x_release_version}/${rockchip_rk356x_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-rockchip_rk356x-${2}.tar.gz
 
 rm -f ${DOWNLOAD_DIR}/boot-rockchip_rk356x-${2}.dd
-wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-x96_x6.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-rockchip_rk356x-${2}.dd
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-x96_x6.dd.gz -O - | gunzip -c | dd bs=512 seek=34 skip=34 of=${DOWNLOAD_DIR}/boot-rockchip_rk356x-${2}.dd
 
 # get different u-boot versions for different rk33xx rockchip versions to have them around
 rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
 mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
 cp ${DOWNLOAD_DIR}/boot-rockchip_rk356x-${2}.dd ${DOWNLOAD_DIR}/boot-extra-${1}/boot-x96_x6.dd
-wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-quartz64-b.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-quartz64-b.dd
-wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-rock3-a.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-rock3-a.dd
-wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-rock3-c.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-rock3-c.dd
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-quartz64-b.dd.gz -O - | gunzip -c | dd bs=512 seek=34 skip=34 of=${DOWNLOAD_DIR}/boot-extra-${1}/boot-quartz64-b.dd
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-rock3-a.dd.gz -O - | gunzip -c | dd bs=512 seek=34 skip=34 of=${DOWNLOAD_DIR}/boot-extra-${1}/boot-rock3-a.dd
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-rock3-c.dd.gz -O - | gunzip -c | dd bs=512 seek=34 skip=34 of=${DOWNLOAD_DIR}/boot-extra-${1}/boot-rock3-c.dd
--- systems/rockchip_rk3588/get-files.sh
+++ systems/rockchip_rk3588/get-files.sh
@@ -7,10 +7,10 @@ rm -f ${DOWNLOAD_DIR}/kernel-rockchip_rk3588-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-rockchip-rk3588-kernel/releases/download/${rockchip_rk3588_release_version}/${rockchip_rk3588_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-rockchip_rk3588-${2}.tar.gz
 
 rm -f ${DOWNLOAD_DIR}/boot-rockchip_rk3588-${2}.dd
-wget -v https://github.com/hexdump0815/linux-rockchip-rk3588-kernel/raw/${rockchip_rk3588_u_boot_binary_version}/misc.r58/u-boot/boot-orange-pi-5.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-rockchip_rk3588-${2}.dd
+wget -v https://github.com/hexdump0815/linux-rockchip-rk3588-kernel/raw/${rockchip_rk3588_u_boot_binary_version}/misc.r58/u-boot/boot-orange-pi-5.dd.gz -O - | gunzip -c | dd bs=512 seek=34 skip=34 of=${DOWNLOAD_DIR}/boot-rockchip_rk3588-${2}.dd
 
 # get different u-boot versions for different rk33xx rockchip versions to have them around
 rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
 mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
 cp ${DOWNLOAD_DIR}/boot-rockchip_rk3588-${2}.dd ${DOWNLOAD_DIR}/boot-extra-${1}/boot-orange-pi-5.dd
-wget -v https://github.com/hexdump0815/linux-rockchip-rk3588-kernel/raw/${rockchip_rk3588_u_boot_binary_version}/misc.r58/u-boot/boot-rock-5b.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-rock-5b.dd
+wget -v https://github.com/hexdump0815/linux-rockchip-rk3588-kernel/raw/${rockchip_rk3588_u_boot_binary_version}/misc.r58/u-boot/boot-rock-5b.dd.gz -O - | gunzip -c | dd bs=512 seek=34 skip=34 of=${DOWNLOAD_DIR}/boot-extra-${1}/boot-rock-5b.dd
