8f8e5149cfe4bb8a0f8531a7d9a71f7ea0cb58dc
hexdump <hexdump0815@googlemail.com>
2023-03-21 23:10:44 +0100

    files: unchanged jack install seems to work on riscv64 now

--- files/extra-files-sidriscv/scripts/install-jack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get -y install qjackctl pulseaudio-module-jack
--- files/extra-files-sidriscv/scripts/remove-jack.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-apt-get -y remove --purge qjackctl pulseaudio-module-jack
-
-apt-get -y auto-remove
