a5032d06442f8a3ae1f83b868f89db5a63757a2c
hexdump <hexdump0815@googlemail.com>
2023-03-26 13:11:13 +0200

    a3u: add active pen of surftab dou w1 as comment
    it looks like the active pen is handled via a separate xinput device for
    the touch screen - add it as a comment so that at least the name is
    documented somewhere

--- systems/atom_x86_with_32bit_uefi/extra-files/etc/lightdm/surftab-duo-w1.sh
+++ systems/atom_x86_with_32bit_uefi/extra-files/etc/lightdm/surftab-duo-w1.sh
@@ -6,4 +6,10 @@ TOUCH_ID=`xinput list | grep "Goodix Capacitive TouchScreen" | grep pointer | se
 # swap the touchscreen cursor x axis
 xinput set-prop $TOUCH_ID "Coordinate Transformation Matrix" -1 0 1 0 1 0 0 0 1
 
+# # find the active pen
+# TOUCH_ID=`xinput list | grep "Goodix Active Pen" | grep pointer | sed 's,.*id=,,g' | awk '{print $1}'`
+#
+# # swap the touchscreen cursor x axis
+# xinput set-prop $TOUCH_ID "Coordinate Transformation Matrix" -1 0 1 0 1 0 0 0 1
+
 exit 0
