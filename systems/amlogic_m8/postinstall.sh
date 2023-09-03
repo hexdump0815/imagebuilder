789a84f8d3f2596e6e1c6d05c8587e9950be2fc1
hexdump <hexdump0815@googlemail.com>
2023-09-03 20:44:11 +0200

    amlogic m8: v6.1.51 - working hdmi, dtb cleanup, threadirqs back
    - kernel is now v6.1.51 with working hdmi output support on meson8 :)
    - add extra xorg config files for it (16bit monitor, swapped dri lima)
    - cleanup the same dtb files in uEnv.ini
    - bring back threadirqs kernel cmdline options as it might still help
      stability on some meson8 systems (not 100% sure yet, also a bit
      unclear due to the early boot kernel warning about it)
    - blacklist qrtr module for now (only useful for qcom devices)

--- systems/amlogic_m8/postinstall.sh
+++ systems/amlogic_m8/postinstall.sh
@@ -2,3 +2,5 @@
 
 # just put some generic xorg.conf there although xorg does not make sense here yet
 cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/13-lima-meson-swapped-dri-nodes.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/31-monitor-16bit-no-dpms.conf etc/X11/xorg.conf.d
