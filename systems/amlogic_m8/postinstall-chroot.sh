131de634a1117458fb509920a5bef384b361a671
hexdump <hexdump0815@googlemail.com>
2023-09-09 23:45:39 +0200

    amlogic m8: keep lightdm with working hdmi

--- systems/amlogic_m8/postinstall-chroot.sh
+++ systems/amlogic_m8/postinstall-chroot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# there is no hdmi supported you, so disable the lightdm display manager for now
-systemctl stop lightdm
-systemctl disable lightdm
+# this is no longer required as hdmi out works now on meson8
+## there is no hdmi supported you, so disable the lightdm display manager for now
+#systemctl stop lightdm
+#systemctl disable lightdm
