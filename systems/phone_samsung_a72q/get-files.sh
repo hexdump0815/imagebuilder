61157c0d0bab6e9ab33651fd8908a261a9c2c793
hexdump <hexdump0815@googlemail.com>
2023-09-29 12:58:37 +0200

    a72q: some small fixes

--- systems/phone_samsung_a72q/get-files.sh
+++ systems/phone_samsung_a72q/get-files.sh
@@ -2,8 +2,8 @@
 
 samsung_a72q_release_version="5.19.0-stb-a72%2B"
 
-rm -f ${DOWNLOAD_DIR}/kernel-samsung_a72q-${2}.tar.gz
-wget -v https://github.com/hexdump0815/linux-mainline-qcom-msm8998-kernel/releases/download/${samsung_a72q_release_version}/${samsung_a72q_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-samsung_a72q-${2}.tar.gz
+rm -f ${DOWNLOAD_DIR}/kernel-phone_samsung_a72q-${2}.tar.gz
+wget -v https://github.com/hexdump0815/linux-mainline-qcom-kernel/releases/download/${samsung_a72q_release_version}/${samsung_a72q_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-phone_samsung_a72q-${2}.tar.gz
 
 # get the firmware and related files to get wifi and other things working - maybe an option for the future ...
 #wget -v https://github.com/hexdump0815/imagebuilder-firmware/raw/main/phone-samsung-a72q-firmware.tar.gz -O ${DOWNLOAD_DIR}/postinstall-${1}/phone-samsung-a72q-firmware.tar.gz
