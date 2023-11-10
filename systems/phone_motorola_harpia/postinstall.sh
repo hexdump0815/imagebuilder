3ff8634e0b152723b2b3cda8878cf42b4c553c88
hexdump <hexdump0815@googlemail.com>
2023-11-10 23:46:52 +0100

    gt510,harpia: display is rotated by default now + boot setup
    the default installation has the display rotated to landscape mode by
    default now plus some extra steps required to put the boot.img and
    initramfs-extra where the bootloader expects them

--- systems/phone_motorola_harpia/postinstall.sh
+++ systems/phone_motorola_harpia/postinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
-# in case display should be rotated - in this case also uncomment
-# the lines in /etc/udev/rules.d/90-android-touch-dev.rules
-#cp -v etc/X11/xorg.conf.d.samples/31-monitor-rotate-right.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/31-monitor-rotate-right.conf etc/X11/xorg.conf.d
+# in case the display should not be rotated, comment out the above line and also
+# comment out the lines in /etc/udev/rules.d/90-android-touch-dev.rules
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
 
 # bullseye and bookworm
@@ -14,8 +14,9 @@ elif [ -f etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manag
   cp -v etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-off etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
 fi
 
-# install the kernel, initramfs, boot.img etc.
-tar xhzf postinstall/boot-and-modules.tar.gz
+# put the boot.img and initramfs-extra where the bootloader expects them
+cp boot/boot.img-harpia-* boot/boot.img
+cp boot/initramfs-extra-* boot/initramfs-extra
 
 # this is required for the msm firmware loader
 mkdir -p lib/firmware/msm-firmware-loader
--- systems/tablet_samsung_gt510/extra-files/scripts/fix-display.sh
+++ systems/tablet_samsung_gt510/extra-files/scripts/fix-display.sh
@@ -3,6 +3,6 @@
 xrandr --display :0 --output DSI-1 --off
 # the short sleep seems to be required with newer kernels ...
 sleep 1
-xrandr --display :0 --output DSI-1 --auto
-# in case the display is rotated
-#xrandr --display :0 --output DSI-1 --auto --rotate right
+xrandr --display :0 --output DSI-1 --auto --rotate right
+# in case the display is not rotated
+#xrandr --display :0 --output DSI-1 --auto
