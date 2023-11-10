840aae0c1df2bf4726181bceef878197b6ae7524
hexdump <hexdump0815@googlemail.com>
2023-11-10 23:41:36 +0100

    gt510,harpia: usethe self-built v6.6.0 kernel
    this is now a kernel built directly on (for instance) the system itself
    instead of using the pmos pmbootstrap framework to build it

--- systems/tablet_samsung_gt510/get-files.sh
+++ systems/tablet_samsung_gt510/get-files.sh
@@ -1,15 +1,10 @@
 # this file is supposed to be sourced by the get-files shell script
 
-# right now i'm still using a postmarketos kernel+initrd=boot.img and will maybe later move to an own boot.img
-
-# get a kernel etc. from a special portmarketos based build
-
-samsung_gt510_pmos_release_version="linux-postmarketos-qcom-msm8916-5.18-r101"
+msm8916_kernel_release_version="6.6.0-stb-410%2B"
 mesa_release_version="22.1.1"
 
-rm -rf ${DOWNLOAD_DIR}/postinstall-${1}
-mkdir -p ${DOWNLOAD_DIR}/postinstall-${1}
-wget -v https://github.com/hexdump0815/pmaports-other/releases/download/${samsung_gt510_pmos_release_version}/boot-and-modules-samsung-gt510.tar.gz -O ${DOWNLOAD_DIR}/postinstall-${1}/boot-and-modules.tar.gz
+rm -f ${DOWNLOAD_DIR}/kernel-tablet_samsung_gt510-${2}.tar.gz
+wget -v https://github.com/hexdump0815/linux-mainline-qcom-kernel/releases/download/${msm8916_kernel_release_version}/${msm8916_kernel_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-tablet_samsung_gt510-${2}.tar.gz
 
 # get the self built fresher mesa
 if [ "${3}" = "bullseye" ] || [ "${3}" = "focal" ]; then
