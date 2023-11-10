73620b5b121725504144453516da1a04e7f4475d
hexdump <hexdump0815@googlemail.com>
2023-11-10 20:36:18 +0100

    gt510: fix-display.sh needs a short sleep now

--- systems/tablet_samsung_gt510/extra-files/scripts/fix-display.sh
+++ systems/tablet_samsung_gt510/extra-files/scripts/fix-display.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 xrandr --display :0 --output DSI-1 --off
+# the short sleep seems to be required with newer kernels ...
+sleep 1
 xrandr --display :0 --output DSI-1 --auto
 # in case the display is rotated
 #xrandr --display :0 --output DSI-1 --auto --rotate right
