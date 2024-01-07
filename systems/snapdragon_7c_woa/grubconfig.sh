94bdfb35834c0fb3d47121f9d0edc7d4ee506131
hexdump <hexdump0815@googlemail.com>
2024-01-07 21:37:30 +0100

    7c-woa: sync grubconfig.sh to used kernel cmdline
    as the regular grub is not yet used on aarch64, but a special old
    version instead grubconfig.sh is not relevant yet, but at least it
    should be in sync to the kernel cmdline used now

--- systems/snapdragon_7c_woa/grubconfig.sh
+++ systems/snapdragon_7c_woa/grubconfig.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# add some extra options to the kernel cmdline
-sed -i 's,GRUB_CMDLINE_LINUX="",GRUB_CMDLINE_LINUX="rootwait fsck.repair=yes net.ifnames=0 ipv6.disable=1 systemd.gpt_auto=0",g' /etc/default/grub
+# add some extra options to the kernel cmdline - not really used here yet as some special grup is used for now
+sed -i 's,GRUB_CMDLINE_LINUX="",GRUB_CMDLINE_LINUX="rootwait fsck.repair=yes net.ifnames=0 ipv6.disable=1 systemd.gpt_auto=0 efi=novamap,noruntime clk_ignore_unused pd_ignore_unused console=tty0 ignore_loglevel noresume deferred_probe_timeout=30 apparmor=0",g' /etc/default/grub
