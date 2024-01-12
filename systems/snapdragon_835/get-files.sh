c183b6fa51d95d07ad9ee792ad83ad4885a7a1bb
hexdump <hexdump0815@googlemail.com>
2024-01-12 22:06:36 +0100

    sd835: move kernel to v6.6 - no major changes otherwise

--- systems/snapdragon_835/get-files.sh
+++ systems/snapdragon_835/get-files.sh
@@ -1,6 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-snapdragon_835_release_version="6.1.0-stb-qcn"
+snapdragon_835_release_version="6.6.9-stb-qcn+"
 
 rm -f ${DOWNLOAD_DIR}/kernel-snapdragon_835-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-qcom-msm8998-kernel/releases/download/${snapdragon_835_release_version}/${snapdragon_835_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-snapdragon_835-${2}.tar.gz
