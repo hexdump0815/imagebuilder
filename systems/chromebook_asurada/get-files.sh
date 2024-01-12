cb2a84aa73bf61a296652b7c66f68247fb4e2e2d
hexdump <hexdump0815@googlemail.com>
2024-01-12 20:36:44 +0100

    asurada,cherry: use close to mainline kernel and cleanup

--- systems/chromebook_asurada/get-files.sh
+++ systems/chromebook_asurada/get-files.sh
@@ -1,11 +1,8 @@
 # this file is supposed to be sourced by the get-files shell script
 
-# use a chromeos based kernel until mainline starts to be useable
-chromebook_asurada_chromeos_release_version="6.1.0-cos-mt9%2B"
-# this will be required when panfrost starts to get useable
-#mesa_release_version="22.1.1"
+chromebook_asurada_release_version="6.1.0-cos-mt9%2B"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_asurada-${2}.tar.gz
-wget -v https://github.com/hexdump0815/linux-chromeos-kernel/releases/download/${chromebook_asurada_chromeos_release_version}/${chromebook_asurada_chromeos_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_asurada-${2}.tar.gz
+wget -v https://github.com/hexdump0815/linux-chromeos-kernel/releases/download/${chromebook_asurada_release_version}/${chromebook_asurada_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_asurada-${2}.tar.gz
 
 ( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_asurada-${2}.tar.gz boot ; mv boot/vmlinux.kpart-* boot-chromebook_asurada-${2}.dd ; rm -rf boot )
--- systems/chromebook_cherry/get-files.sh
+++ systems/chromebook_cherry/get-files.sh
@@ -1,11 +1,8 @@
 # this file is supposed to be sourced by the get-files shell script
 
-# use a chromeos based kernel until mainline starts to be useable
-chromebook_cherry_chromeos_release_version="5.10.83-chr-r98%2B"
-# this will be required when panfrost starts to get useable
-#mesa_release_version="22.1.1"
+chromebook_cherry_release_version="6.1.0-cos-mt9%2B"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_cherry-${2}.tar.gz
-wget -v https://github.com/hexdump0815/linux-chromeos-kernel/releases/download/${chromebook_cherry_chromeos_release_version}/${chromebook_cherry_chromeos_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_cherry-${2}.tar.gz
+wget -v https://github.com/hexdump0815/linux-chromeos-kernel/releases/download/${chromebook_cherry_release_version}/${chromebook_cherry_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_cherry-${2}.tar.gz
 
 ( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_cherry-${2}.tar.gz boot ; mv boot/vmlinux.kpart-* boot-chromebook_cherry-${2}.dd ; rm -rf boot )
