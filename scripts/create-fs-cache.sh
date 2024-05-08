0ae836eed5e93d3fbb07b073a3638638c08e578f
hexdump <hexdump0815@googlemail.com>
2024-05-08 06:51:00 +0200

    scripts: remove unused old sidriscv code

--- scripts/create-fs-cache.sh
+++ scripts/create-fs-cache.sh
@@ -142,19 +142,6 @@ if [ ! -d ${BUILD_ROOT_CACHE} ]; then
     cp ${WORKDIR}/files/sidriscv-${BOOTSTRAP_ARCH}-sources.list ${BUILD_ROOT_CACHE}/etc/apt/sources.list
     # parse in the proper debian version
     sed -i "s,DEBIANVERSION,sid,g" ${BUILD_ROOT_CACHE}/etc/apt/sources.list
-#    apt -y install debootstrap debian-ports-archive-keyring --no-install-recommends
-#    LANG=C debootstrap --arch=${BOOTSTRAP_ARCH} --foreign --keyring /usr/share/keyrings/debian-ports-archive-keyring.gpg --include=debian-ports-archive-keyring --no-check-certificate --variant=minbase sid ${BUILD_ROOT_CACHE} http://deb.debian.org/debian-ports
-#    # exit if debootstrap failed for some reason
-#    if [ "$?" != "0" ]; then
-#      echo ""
-#      echo "error while running debootstrap - giving up"
-#      echo ""
-#      rm -rf ${BUILD_ROOT_CACHE}
-#      exit 1
-#    fi
-#    cp ${WORKDIR}/files/sidriscv-${BOOTSTRAP_ARCH}-sources.list ${BUILD_ROOT_CACHE}/etc/apt/sources.list
-#    # this seems to be required to not need special options to apt commands when installing from the ports repo
-#    echo 'APT::Get::AllowUnauthenticated "true";' > ${BUILD_ROOT_CACHE}/etc/apt/apt.conf.d/99unauthenticated
   else
     echo ""
     echo "${2} is not supported as release - giving up!"
