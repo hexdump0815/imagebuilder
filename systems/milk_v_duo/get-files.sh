a90fac61061ecae9d8b95898e36921e1873081da
hexdump <hexdump0815@googlemail.com>
2024-05-25 14:25:29 +0200

    duo: proper kernel download

--- systems/milk_v_duo/get-files.sh
+++ systems/milk_v_duo/get-files.sh
@@ -1,12 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-milk_v_duo_release_version="to-be-done"
-milk_v_duo_boot_version="to-be-done"
+milk_v_duo_release_version="5.10.4-mkv-duo+"
 
-#rm -f ${DOWNLOAD_DIR}/kernel-milk_v_duo-${2}.tar.gz
-#wget -v https://github.com/hexdump0815/linux-cvitek-cv18xx-kernel/releases/download/${milk_v_duo_release_version}/${milk_v_duo_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-milk_v_duo-${2}.tar.gz
-
-#rm -f ${DOWNLOAD_DIR}/boot-milk_v_duo-${2}.dd
-#wget -v to-be-done
-
-echo "nothing done here yet - still to be done"
+rm -f ${DOWNLOAD_DIR}/kernel-milk_v_duo-${2}.tar.gz
+wget -v https://github.com/hexdump0815/linux-cvitek-cv18xx-kernel/releases/download/${milk_v_duo_release_version}/${milk_v_duo_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-milk_v_duo-${2}.tar.gz
