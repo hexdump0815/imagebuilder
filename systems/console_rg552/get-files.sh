25203e2ed9a22fa6eb437ad562c00e7c06a8d3ac
luk <92756992+thenameisluk@users.noreply.github.com>
2024-12-23 00:38:36 +0100

    rg552 -> rg5 because

--- systems/console_rg552/get-files.sh
+++ systems/console_rg552/get-files.sh
@@ -4,4 +4,4 @@ rm -f ${DOWNLOAD_DIR}/boot-console_rg552-aarch64.dd
 wget -v https://github.com/thenameisluk/u-boot-rk3xxx-bootloader/releases/download/anbernic-rg552_gen1/boot-console_rg552-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-console_rg552-aarch64.dd
 
 rm -f ${DOWNLOAD_DIR}/kernel-console_rg552-${2}.tar.gz
-wget -v https://github.com/thenameisluk/linux-rk3xxx-kernel/releases/download/6.12.0-stb-rg552%2B/6.12.0-stb-rg552+.tar.gz -O ${DOWNLOAD_DIR}/kernel-console_rg552-${2}.tar.gz
+wget -v https://github.com/thenameisluk/linux-rk3xxx-kernel/releases/download/6.12.0-stb-rg5%2B/6.12.0-stb-rg5+.tar.gz -O ${DOWNLOAD_DIR}/kernel-console_rg552-${2}.tar.gz
