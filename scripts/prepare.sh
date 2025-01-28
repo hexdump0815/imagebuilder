8f686c609e1da9003962e423307c9f6ec0828d85
hexdump <hexdump0815@googlemail.com>
2025-01-28 09:28:33 +0100

    scripts: remove liblz4-tool from prepare.sh script
    it does not seem to exist in debian trixie right now and its part of the
    lz4 package which is installed by default on all images now anyway, so
    no extra install required

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,7 +3,7 @@
 # install some packages required for the imagebuilder framework
 #
 
-apt-get -y install debootstrap liblz4-tool btrfs-progs
+apt-get -y install debootstrap btrfs-progs
 if [ "$(uname -m)" != "riscv64" ]; then
   apt-get -y install vboot-kernel-utils cgpt
 fi
