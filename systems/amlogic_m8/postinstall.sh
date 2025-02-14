fa867acf8bff7409abc8a72240bf16ce425f5b03
hexdump <hexdump0815@googlemail.com>
2025-02-14 10:35:48 +0100

    amlogic_m8: disable lima gpu driver by default
    it seems to have problems on at least a few boxes, so lets better
    disable it by default, so that xorg will come up reliable at least ...

--- systems/amlogic_m8/postinstall.sh
+++ systems/amlogic_m8/postinstall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # just put some generic xorg.conf there although xorg does not make sense here yet
-cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
-cp -v etc/X11/xorg.conf.d.samples/13-lima-meson-swapped-dri-nodes.conf etc/X11/xorg.conf.d
+# as the lima gpu driver seems to have problems on some boxes, lets disable it by default
+cp -v etc/X11/xorg.conf.d.samples/11-modesetting-no-glamor.conf etc/X11/xorg.conf.d
+#cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
+#cp -v etc/X11/xorg.conf.d.samples/13-lima-meson-swapped-dri-nodes.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/31-monitor-16bit-no-dpms.conf etc/X11/xorg.conf.d
