28a324c165a32d9b6172a06968810bbd07c495ae
hexdump <hexdump0815@googlemail.com>
2025-02-18 22:32:28 +0100

    scripts,7c_woa,sdm835: use the regular grub also for arm64
    so far some old hacked together grub from an old aarch64-laptops image
    was used to boot the woa arm64/aarch64 systems as the grub version in
    older debian and ubuntu releases was still not working well on aarch64,
    but it looks like the regular distribution grub is ready now and can be
    used
    
    tested to boot on a samsung galaxy book go (for 7c) and a lenovo mixx
    630 (for sdm835) but it looks like the bios on those devices is very
    picky and it does not seem to boot to grub properly with some usb
    devices and sometimes it is also required to fix the gpt partition table
    by just opening it with fdisk and saving it for the bios to see the
    device as a bootable device ... sometimes multiple boot tries are
    required to get the devices recognized, sometimes it is worth to try
    different usb ports or use a hub inbetween (that is not related to the
    new grub but more to how broken those old arm64 efi bios implementations
    are)
    
    the boot menu entries are for now created as custom entries via
    /etc/grub.d/40_custom for the kernel in the image and new entries will
    have to be added by hand there followed by a update-grub - this approach
    should work well for both debian and ubuntu ... in case someone has a
    lot of time to create better integrations via 10_linux similar to how it
    is done for x86 for both debian and ubuntu i'm happy to see pull
    requests for such an approach ...

--- systems/snapdragon_7c_woa/grubconfig.sh
+++ systems/snapdragon_7c_woa/grubconfig.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-# add some extra options to the kernel cmdline - not really used here yet as some special grup is used for now
-sed -i 's,GRUB_CMDLINE_LINUX="",GRUB_CMDLINE_LINUX="rootwait fsck.repair=yes net.ifnames=0 ipv6.disable=1 systemd.gpt_auto=0 efi=novamap,noruntime clk_ignore_unused pd_ignore_unused console=tty0 ignore_loglevel noresume deferred_probe_timeout=30 apparmor=0",g' /etc/default/grub
+# add some extra options to the kernel cmdline - not used yet
+sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="rootwait ro console=tty0 fsck.repair=yes net.ifnames=0 ipv6.disable=1 noresume apparmor=0 efi=novamap,noruntime clk_ignore_unused pd_ignore_unused deferred_probe_timeout=30"/g' /etc/default/grub
+# do not boot any dtb entry automatically - the user has to decide according to the device used
+sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=-1/g' /etc/default/grub
+
+# the following will parse the proper kernel version and the proper uuids of the
+# boot and root partitions into the provided custom grub menu entries
+# the velvet custom config is initially named differently and here renamed to its
+# regular name to not result in a conflict during the initial grub installation
+mv -vf /etc/grub.d/40_custom.velvet /etc/grub.d/40_custom
+BOOT_UUID=`blkid | grep /dev/loop0p2 | sed 's,.* UUID=",,g;s," .*,,g'`
+ROOT_UUID=`blkid | grep /dev/loop0p3 | sed 's,.* UUID=",,g;s," .*,,g'`
+KERNEL_VERSION=`ls /boot/*Image-* | sed 's,.*Image-,,g' | sort -u`
+sed -i "s,BOOT_UUID,${BOOT_UUID},g" /etc/grub.d/40_custom
+sed -i "s,ROOT_UUID,${ROOT_UUID},g" /etc/grub.d/40_custom
+sed -i "s,KERNEL_VERSION,${KERNEL_VERSION},g" /etc/grub.d/40_custom
