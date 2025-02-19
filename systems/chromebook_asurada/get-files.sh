323ef1c9f76b288b3f3278d04b0e466adeb0c706
hexdump <hexdump0815@googlemail.com>
2025-02-19 20:56:37 +0100

    asurada,corsola: take kernel release from the proper repo for cbm

--- systems/chromebook_asurada/get-files.sh
+++ systems/chromebook_asurada/get-files.sh
@@ -3,6 +3,6 @@
 chromebook_asurada_release_version="6.12.12-stb-cbm%2B"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_asurada-${2}.tar.gz
-wget -v https://github.com/hexdump0815/linux-chromeos-kernel/releases/download/${chromebook_asurada_release_version}/${chromebook_asurada_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_asurada-${2}.tar.gz
+wget -v https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/${chromebook_asurada_release_version}/${chromebook_asurada_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_asurada-${2}.tar.gz
 
 ( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_asurada-${2}.tar.gz boot ; mv boot/vmlinux.kpart-* boot-chromebook_asurada-${2}.dd ; rm -rf boot )
--- systems/chromebook_cherry/get-files.sh
+++ systems/chromebook_cherry/get-files.sh
@@ -3,6 +3,6 @@
 chromebook_cherry_release_version="6.12.12-stb-cbm%2B"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_cherry-${2}.tar.gz
-wget -v https://github.com/hexdump0815/linux-chromeos-kernel/releases/download/${chromebook_cherry_release_version}/${chromebook_cherry_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_cherry-${2}.tar.gz
+wget -v https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/${chromebook_cherry_release_version}/${chromebook_cherry_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_cherry-${2}.tar.gz
 
 ( cd ${DOWNLOAD_DIR} ; tar xzf kernel-chromebook_cherry-${2}.tar.gz boot ; mv boot/vmlinux.kpart-* boot-chromebook_cherry-${2}.dd ; rm -rf boot )
