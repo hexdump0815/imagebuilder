3c67df7d82415d4ebff0e051be51b24d996b4ad8
hexdump <hexdump0815@googlemail.com>
2025-02-21 11:38:13 +0100

    oak,kukui,corsola,asurada,cherry: newer kernel for early on display

--- systems/chromebook_asurada/get-files.sh
+++ systems/chromebook_asurada/get-files.sh
@@ -1,6 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-chromebook_asurada_release_version="6.12.12-stb-cbm%2B"
+chromebook_asurada_release_version="6.12.15-stb-cbm%2B"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_asurada-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/${chromebook_asurada_release_version}/${chromebook_asurada_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_asurada-${2}.tar.gz
--- systems/chromebook_cherry/get-files.sh
+++ systems/chromebook_cherry/get-files.sh
@@ -1,6 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-chromebook_cherry_release_version="6.12.12-stb-cbm%2B"
+chromebook_cherry_release_version="6.12.15-stb-cbm%2B"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_cherry-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/${chromebook_cherry_release_version}/${chromebook_cherry_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_cherry-${2}.tar.gz
