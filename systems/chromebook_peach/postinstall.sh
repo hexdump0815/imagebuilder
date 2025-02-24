b92a0efb7735cf22f48fc9eaf7bf5d1c45356d0f
hexdump <hexdump0815@googlemail.com>
2025-02-24 18:33:55 +0100

    peach: add xorg.conf copy, add note to disabled panfrost blacklist

--- systems/chromebook_peach/postinstall.sh
+++ systems/chromebook_peach/postinstall.sh
@@ -6,6 +6,10 @@
 #echo "load-module module-alsa-sink device=sysdefault" >> etc/pulse/default.pa
 #echo "#load-module module-alsa-source device=sysdefault" >> etc/pulse/default.pa
 
+cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/31-monitor.conf etc/X11/xorg.conf.d
+
 # lets better use the dpms suspend free version here, as there were some drm unblank kernel errors
 # bookworm and trixie
 if [ -f etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled-no-dpms-suspend ]; then
