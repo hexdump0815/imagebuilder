870d21c06711d154d8afecb9358d28cb957a49a9
hexdump <hexdump0815@googlemail.com>
2025-02-24 18:38:39 +0100

    nyan: enable nouveau gpu driver - it seems to work to some degree again
    surprise, surprise: i did not expect this, but it looks like the nouveau
    gpu driver is working again together with newer kernel and mesa
    versions, so lets enable it again by default on nyan
    
    i'm not sure if it will work well in all situations, but at least
    glmark2 is running through fine ...
    
    the commented out pstate tuning value in rc.local gets moved from "06"
    to "04" as i saw instability with 06 ... maybe some experimentation is
    required to find out what works reliable and what not

--- systems/chromebook_nyan/postinstall.sh
+++ systems/chromebook_nyan/postinstall.sh
@@ -3,14 +3,10 @@
 # maybe this helps to improve the suspend situation a bit ...
 echo "SuspendState=freeze" >> etc/systemd/sleep.conf
 
-# the v5.4 kernel does not work well together with the nouveau gpu driver, so disable it for now
-cp -v etc/X11/xorg.conf.d.samples/11-modesetting-no-glamor.conf etc/X11/xorg.conf.d
-# in case the newer xorg server is used which is required for the nouveau gpu driver to work
-# with the v5.10 kernel then this is required to make the chromebook keyboard work - this is
-# just a hack for now and should be solved better in the future
-#cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
-#cp -v etc/X11/xorg.conf.d.samples/51-newer-xorg-chromebook-kbd.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/13-nouveau-nyan.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/31-monitor.conf etc/X11/xorg.conf.d
 
 # bookworm and trixie
 if [ -f etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml-enabled ]; then
