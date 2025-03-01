4608a0f0f13e6c79880a1272de33f4142cdb1bff
hexdump <hexdump0815@googlemail.com>
2025-03-02 00:13:41 +0100

    h616: use the h618 box +60mv dtb as default from now on
    nowadays its more likely to get such a box then a h313 one as the old
    default was ... also there are so many different h313 box variants that
    its not that likely the the dtb used will really work for many of them
    
    the bootblocks get adjusted accordingly to use the proper one for h618
    boxes by default as well

--- systems/allwinner_h616/get-files.sh
+++ systems/allwinner_h616/get-files.sh
@@ -7,15 +7,15 @@ rm -f ${DOWNLOAD_DIR}/kernel-allwinner_h616-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-allwinner-h6-kernel/releases/download/${allwinner_h616_release_version}/${allwinner_h616_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-allwinner_h616-${2}.tar.gz
 
 rm -f ${DOWNLOAD_DIR}/boot-allwinner_h616-${2}.dd
-wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${allwinner_h616_uboot_version}/boot-allwinner_h616-tx6s-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-allwinner_h616-${2}.dd
+# get the warpme minimyth2 based axp313a boot block used as well for many h618 boxes
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-allwinner-h6-kernel/raw/24b1d4651f39f1a690debf06f23a9e35dae775e2/misc.616/u-boot/boot-h618-axp313a.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-allwinner_h616-${2}.dd
 
 # get different u-boot versions for different veyron versions to have them around
 rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
 mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
-cp ${DOWNLOAD_DIR}/boot-allwinner_h616-${2}.dd ${DOWNLOAD_DIR}/boot-extra-${1}/boot-allwinner_h616-tx6s.dd
+cp ${DOWNLOAD_DIR}/boot-allwinner_h616-${2}.dd ${DOWNLOAD_DIR}/boot-extra-${1}/boot-allwinner_h616-axp313a.dd
+wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${allwinner_h616_uboot_version}/boot-allwinner_h616-tx6s-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-allwinner_h616-tx6s.dd
 wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${allwinner_h616_uboot_version}/boot-allwinner_h616-t95-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-allwinner_h616-t95.dd
 wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${allwinner_h616_uboot_version}/boot-allwinner_h616-x96mate-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-allwinner_h616-x96mate.dd
 wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${allwinner_h616_uboot_version}/boot-allwinner_h616-opizero2-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-allwinner_h616-opizero2.dd
-# get the warpme minimyth2 based axp313a boot block as well for h618 experiments
-wget -v https://github.com/hexdump0815/linux-mainline-and-mali-allwinner-h6-kernel/raw/24b1d4651f39f1a690debf06f23a9e35dae775e2/misc.616/u-boot/boot-h618-axp313a.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-allwinner_h616-axp313a.dd
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-allwinner-h6-kernel/raw/24b1d4651f39f1a690debf06f23a9e35dae775e2/misc.616/u-boot/boot-h313-lpddr3.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-allwinner_h313-lpddr3.dd
