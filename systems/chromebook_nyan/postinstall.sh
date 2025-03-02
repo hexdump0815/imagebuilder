c4fe6b1975e42b63dad9701e35d9e8ad73d921e1
hexdump <hexdump0815@googlemail.com>
2025-03-02 23:24:56 +0100

    nyan: disable the nouveau gpu driver by default again
    with it enabled firefox is crashing, so lets better keep it disabled by
    default again ...

--- systems/chromebook_nyan/postinstall.sh
+++ systems/chromebook_nyan/postinstall.sh
@@ -3,8 +3,12 @@
 # maybe this helps to improve the suspend situation a bit ...
 echo "SuspendState=freeze" >> etc/systemd/sleep.conf
 
-cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
-cp -v etc/X11/xorg.conf.d.samples/13-nouveau-nyan.conf etc/X11/xorg.conf.d
+cp -v etc/X11/xorg.conf.d.samples/11-modesetting-no-glamor.conf etc/X11/xorg.conf.d
+# the above line disables gpu support in xorg as with it enabled firefox is crashing
+# to enable the gpu support please comment out the line above and uncomment the two
+# lines below
+#cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
+#cp -v etc/X11/xorg.conf.d.samples/13-nouveau-nyan.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/31-monitor.conf etc/X11/xorg.conf.d
 
