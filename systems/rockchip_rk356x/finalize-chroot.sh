c2cb1a33f36993d60ccb018a7d8156d3d777f1c1
hexdump <hexdump0815@googlemail.com>
2025-03-05 20:57:38 +0100

    rk356x: do not strip efi partiitons for x96 x6 bootblocks
    the efi partition table entries of the x96 x6 bootblocks seem to be
    expected somehow by the legacy bootloader on the x96 x6 box, so do not
    strip them when downloading the bootblock
    
    with this the (non working) workaround via finalize-chroot.sh can go

--- systems/rockchip_rk356x/finalize-chroot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# rewrite the x96 x6 bootblocks as they contain some efi partition info which
-# seems to be expected somehow by the legacy bootloader on the x96 x6 box - it
-# looks like this efi partition info gets overwritten when the new partition
-# table gets created via fdisk in create-image.sh
-dd if=/boot/extra/boot-x96_x6.dd of=/dev/loop0 bs=512 seek=1 skip=1
--- systems/rockchip_rk356x/get-files.sh
+++ systems/rockchip_rk356x/get-files.sh
@@ -7,7 +7,7 @@ rm -f ${DOWNLOAD_DIR}/kernel-rockchip_rk356x-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/releases/download/${rockchip_rk356x_release_version}/${rockchip_rk356x_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-rockchip_rk356x-${2}.tar.gz
 
 rm -f ${DOWNLOAD_DIR}/boot-rockchip_rk356x-${2}.dd
-wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-x96_x6.dd.gz -O - | gunzip -c | dd bs=512 seek=34 skip=34 of=${DOWNLOAD_DIR}/boot-rockchip_rk356x-${2}.dd
+wget -v https://github.com/hexdump0815/linux-mainline-and-mali-rockchip-rk33xx-kernel/raw/${rockchip_rk356x_u_boot_binary_version}/misc.r56/u-boot/boot-x96_x6.dd.gz -O - | gunzip -c | dd bs=512 seek=1 skip=1 of=${DOWNLOAD_DIR}/boot-rockchip_rk356x-${2}.dd
 
 # get different u-boot versions for different rk33xx rockchip versions to have them around
 rm -rf ${DOWNLOAD_DIR}/boot-extra-${1}
