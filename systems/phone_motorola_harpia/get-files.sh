0ab10941b71f933d8c5eb268d404e038f676468a
hexdump <hexdump0815@googlemail.com>
2025-03-24 08:00:44 +0100

    gt510,harpia: newer kernel adding missing pmos msm8916 patches

--- systems/phone_motorola_harpia/get-files.sh
+++ systems/phone_motorola_harpia/get-files.sh
@@ -1,6 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-msm8916_kernel_release_version="6.12.12-stb-410%2B"
+msm8916_kernel_release_version="6.12.20-stb-410%2B"
 
 rm -f ${DOWNLOAD_DIR}/kernel-phone_motorola_harpia-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-qcom-kernel/releases/download/${msm8916_kernel_release_version}/${msm8916_kernel_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-phone_motorola_harpia-${2}.tar.gz
