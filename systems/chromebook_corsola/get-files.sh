39fd335ea2ea4bf34cfc8811f47021769f4d735e
hexdump <hexdump0815@googlemail.com>
2025-03-24 08:01:29 +0100

    corsola,kukui: newer kernel with external display support

--- systems/chromebook_corsola/get-files.sh
+++ systems/chromebook_corsola/get-files.sh
@@ -1,7 +1,7 @@
 # this file is supposed to be sourced by the get-files shell script
 
 # use a chromeos based kernel until mainline starts to be useable
-chromebook_corsola_chromeos_release_version="6.12.15-stb-cbm%2B"
+chromebook_corsola_chromeos_release_version="6.12.18-stb-cbm%2B"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_corsola-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/${chromebook_corsola_chromeos_release_version}/${chromebook_corsola_chromeos_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_corsola-${2}.tar.gz
--- systems/chromebook_kukui/get-files.sh
+++ systems/chromebook_kukui/get-files.sh
@@ -1,6 +1,6 @@
 # this file is supposed to be sourced by the get-files shell script
 
-chromebook_kukui_release_version="6.12.15-stb-cbm%2B"
+chromebook_kukui_release_version="6.12.18-stb-cbm%2B"
 
 rm -f ${DOWNLOAD_DIR}/kernel-chromebook_kukui-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/releases/download/${chromebook_kukui_release_version}/${chromebook_kukui_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-chromebook_kukui-${2}.tar.gz
