c7683c187efe9100dba32c77283f14296af5e91f
hexdump <hexdump0815@googlemail.com>
2025-03-26 21:53:54 +0100

    amlogic_m8: add todo item to the shorten-filenames.sh script
    this functionality should be added whenever there is some time for it as
    otherwise disabling the lima gpu driver via blacklisting it in
    modprobe.d will not work as the rebuilt initramfs is not being used, but
    the uInitrd built from it and that has to be rebuilt each time the
    initramfs gets rebuilt - so lets hook this in here ...

--- systems/amlogic_m8/extra-files/boot/shorten-filenames.sh
+++ systems/amlogic_m8/extra-files/boot/shorten-filenames.sh
@@ -3,6 +3,10 @@
 # the legacy u-boot of the amlogic s8xx can only handle files up to 23 char length
 # so we have shorten some filenames to be on the safe side with the version appended
 #
+# TODO: this should actually be adjusted, so that it will rebuild the uInitrd from
+#       the initramfs image before shortening the filenames, so that an updated
+#       initramfs actually gets used as on this platform the uInitrd is used and
+#       not the initramfs image
 
 cd $(dirname $0)
 for i in uImage-* ; do
